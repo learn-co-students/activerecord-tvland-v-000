751754a32243ad1009969a83f87b3a5b1fff3ca8
jakebrady5 <jakebrady5@gmail.com>
2015-12-06 20:03:50 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    fullname = []
+    fullname << self.first_name
+    fullname << self.last_name
+    fullname.join(" ")
+  end
+
+  def list_roles
+    role_list = []
+
+    role_list << self.characters.first.name
+    role_list << "-"
+    role_list << self.shows.first.name
+
+    role_list.join(" ")
+  end
   
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
+
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    say_what = []
+    say_what << self.name
+    say_what << "always says:"
+    say_what << self.catchphrase
+    say_what.join(" ")
+  end
   
 end
