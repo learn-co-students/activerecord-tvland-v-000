4964a481a883ac5538d4b2b14b66f7a6d29d9eb5
mmmeyers <marsi286@gmail.com>
2015-12-21 12:20:35 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    character_array = []
+    full_list = self.characters
+    full_list.map do |x|
+      character_array << x.name 
+    end
+    character_array
+
+    show_array = []
+    show_list = self.shows 
+    show_list.map do |x|
+      show_array << x.name
+    end
+    show_array
+
+    one = character_array.join()
+    another = show_array.join()
+    "#{one} - #{another}"
+  end
 end
