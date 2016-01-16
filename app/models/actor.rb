2b507aec93ded641691768d96348bdd60561ed18
Tracy <tracetrace@gmail.com>
2016-01-16 23:58:16 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    roles = self.characters.map {|character| "#{character.name} - #{character.show.name}"}
+    roles.join("-")
+  end
+
+
 end
---
+++ db/migrate/007_add_actor_id_to_character.rb
@@ -0,0 +1,7 @@
+class AddActorIdToCharacter < ActiveRecord::Migration
+
+  def change
+    add_column :characters, :actor_id, :integer
+  end
+
+end
