fa7471399b692a6eb9cc29b2c928cd626bfb8b32
coltonstaab1 <colton.staab@gmail.com>
2016-01-20 22:40:18 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+	has_many :characters  
+	has_many :shows, through: :characters
+
+	def full_name
+		"#{self.first_name} #{self.last_name}"
+	end
+
+	def list_roles
+		characters.collect do |character|
+			"#{character.name} - #{character.show.name}"
+		end.join("")
+	end
+
+	def build_show(hash)
+
+	end
+
 end
