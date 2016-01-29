bc2f01d13d01ebf7aa1ca69767b83e3be2dfdcc8
Tsundu <osmentdan@gmail.com>
2016-01-29 16:38:16 -0500

    added models, migrations and methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  has_many :networks, through: :shows 
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+
+ def build_network(opt ={})
+     new_net = Network.new(opt)
+
+ end
+
+
 end
---
+++ db/migrate/006_create_associations.rb
@@ -0,0 +1,5 @@
+class CreateAssociations < ActiveRecord::Migration 
+ def change 
+  add_column :characters, :actor_id, :integer  
+ end
+end 
