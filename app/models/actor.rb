7a099670b5dc41e7496c73a8256b17cbc9998db1
drywaterr24 <angelolea24@gmail.com>
2016-03-01 12:29:33 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    until roles.count == self.shows.count
+      roles << "#{self.characters[roles.count].name} - #{self.characters[roles.count].show.name}"
+    end
+    roles[0]
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :shows
+  belongs_to :network
+
+  def build_network(hash)
+    self.network = Network.new(hash)
+  end
 end
