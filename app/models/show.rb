b00c737cea6cff70d6c6900a87c3cef89dfb39c3
drywaterr24 <angelolea24@gmail.com>
2016-03-01 12:32:46 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many :actors, through: :shows
+  has_many :actors, through: :characters
   belongs_to :network
 
   def build_network(hash)
