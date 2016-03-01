aa3a0371713c70b593a060b73acc9fc22f2851b6
fiveinfinity <jlachance1@gmail.com>
2016-03-01 14:27:21 -0600

    spec passing

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(hash)
+    network = Network.new(hash)
+    self.network = network
+  end
 end
