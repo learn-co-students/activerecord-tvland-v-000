3fd5732b8b5d24d7daf7af0cb29cb47a5d09cc2b
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-03-30 09:50:30 -0700

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,9 @@
-class AddCharacters < ActiveRecord::Migration
+class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.text :name
       t.integer :show_id
+      t.integer :actor_id
     end
   end
 end
