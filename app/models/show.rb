5cf59ef1670bede599d74ab21f514f62119e7766
bedwardsphoto <brian@brianedwardsphotography.com>
2016-03-30 00:28:03 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,6 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
 end
---
+++ db/migrate/006_add_catchphrase_column_to_character.rb
@@ -0,0 +1,6 @@
+class AddCatchphraseColumnToCharacter < ActiveRecord::Migration
+
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+end
