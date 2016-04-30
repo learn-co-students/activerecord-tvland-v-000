d435288f0177fec6a4e1e1f3ba6accd9cafc798a
mhnguyen289 <mhnguyen289@gmail.com>
2016-04-30 02:00:50 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each do |char|
+      roles << char.name
+      roles << char.show.name
+    end
+    roles.join(" - ")
+  end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,6 @@
 class Network < ActiveRecord::Base
   has_many :shows
-
+  
   def sorry
     "We're sorry about passing on John Mullaney's pilot"
   end
