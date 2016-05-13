5cbc5e1b760e5b8d76121a57d1c3d2c1d21c8c5d
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-05-13 02:13:38 -0700

    Done.

--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -6,6 +6,8 @@ class CreateShows < ActiveRecord::Migration
     create_table :shows do |t|
       #primary key of :id is created for us!
       t.string :name
+      t.string :day
+      t.string :season
     end
   end
 end
