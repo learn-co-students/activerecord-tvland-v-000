286a8bb4f92012e10777d84f4ba8443a6877dd69
jchu4483 <jchu4483@gmail.com>
2016-05-25 16:03:38 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+#   Associate the Actor model with the Character and Show model.
+#   An actor should have many characters and many shows through characters.
+  has_many :characters
+  has_many :shows,  through: :characters
+
+    def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+
 end
