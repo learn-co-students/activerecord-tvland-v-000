293805848875ba713a2e3b5a30edd14d4d8ccc16
m-walker <michaelwalker610@gmail.com>
2016-05-27 15:58:43 -0400

    Done.

--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -4,7 +4,6 @@
 class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
-      #primary key of :id is created for us!
       t.string :name
     end
   end
---
+++ db/migrate/010_add_season_to_show.rb
@@ -0,0 +1,5 @@
+class AddSeasonToShow < ActiveRecord::Migration
+  def change
+    add_column :shows, :season, :string
+  end
+end
