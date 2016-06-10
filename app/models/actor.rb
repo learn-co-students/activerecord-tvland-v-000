faf8a450a694d27f77908f2383ecea802dee15ba
Christian Whitesides <whitesidescr@gmail.com>
2016-06-10 17:56:59 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,23 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters = self.characters.map do |character|
-      [character.show_id, character.name]
-    end
-
-    shows = self.shows.map do |show|
-      [show.id, show.name]
-    end
-
-    result = []
-    characters.each do |character|
-      shows.each do |show|
-        if show[0] == character[0]
-          result << "#{character[1]} - #{show[1]}"
-        end
-      end
-    end
-    result
+    self.characters.map { |character| "#{character.name} - #{character.show.name}"}
   end
 
 end
