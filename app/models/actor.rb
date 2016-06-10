d04e385f62606a3ee6c421d65ddbde00361019da
Christian Whitesides <whitesidescr@gmail.com>
2016-06-10 17:37:36 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,29 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters = self.characters.map do |character|
+      [character.show_id, character.name]
+    end
+
+    shows = self.shows.map do |show|
+      [show.id, show.name]
+    end
+
+    result = []
+    characters.each do |character|
+      shows.each do |show|
+        if show[0] == character[0]
+          result << "#{character[1]} - #{show[1]}"
+        end
+      end
+    end
+    result
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(hash)
+    show = Show.create(hash)
+    show.characters << self
+  end
+
+  def build_network
+  end
 end
