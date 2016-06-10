41ec0aeb4d40904f45ad64c35cf117cc93204714
Christian Whitesides <whitesidescr@gmail.com>
2016-06-10 17:47:20 -0600

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,12 +5,4 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-
-  def build_show(hash)
-    show = Show.create(hash)
-    show.characters << self
-  end
-
-  def build_network
-  end
 end
