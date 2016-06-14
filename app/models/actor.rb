f17167dcbcc1e56cc9bfea730541d5817bfead69
jwolfe890 <jwolfe890@gmail.com>
2016-06-14 10:03:26 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+  def create
+    emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
+  end 
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end 
+
   
 end
