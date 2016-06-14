05c32282050afe783dddb6e6e2ee8c29878f1fa6
jwolfe890 <jwolfe890@gmail.com>
2016-06-14 10:04:11 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,9 +4,9 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
 
-  def create
-    emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
-  end 
+  # def create
+  #   emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
+  # end 
 
   def full_name
     "#{self.first_name} #{self.last_name}"
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,11 +1,10 @@
 class Character < ActiveRecord::Base
 
-has_many :actors
 belongs_to :actor  
 belongs_to :show 
 
-def create
-  khaleesi = Character.new(:name => "Khaleesi")
-end 
+# def create
+#   khaleesi = Character.new(:name => "Khaleesi")
+# end 
   
 end
