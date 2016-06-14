d29b99f63798779b0e89ae1a61eb54ecc92009d0
jwolfe890 <jwolfe890@gmail.com>
2016-06-14 14:18:05 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,20 +10,12 @@ def say_that_thing_you_say
   "Steve Urkel always says: #{self.catchphrase}"
 end
 
-def build_show(name_value)
-  self.shows << show 
-  name_value = show.name
-end
-
+def build_show(name: = "Jules Cobb")
+  show = show.build_show(name: = "Jules Cobb") 
+end 
 # def build_network
-
 # end 
-
-
     # jules_cobb = Character.new(:name => "Jules Cobb")
     # jules_cobb.build_show(:name => "Cougar Town")
     # expect(jules_cobb.show.name).to eq("Cougar Town")
-
-
-  
 end
