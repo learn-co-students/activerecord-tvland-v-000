07231859c4ca76d2ab1ffb60b9cfea769e8344bd
Jason <jasonkwong11@gmail.com>
2016-06-27 17:35:44 -0400

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #"Khaleesi - Game of Thrones"
+    self.characters.map do |character_object|
+    "#{character_object.name} - #{character_object.show.name}"
+    end
+  end
 end
