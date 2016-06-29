7dff4b40f8baeffb65c63b08aa86b0d2b1fc4c44
ndaniele <n.daniele@gmail.com>
2016-06-29 12:57:46 -0400

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,11 +1,9 @@
 class Network < ActiveRecord::Base
   has_many :shows
-  has_many :characters, through: :shows
 
   def sorry
     "We're sorry about passing on John Mullaney's pilot"
   end
 
-  def build_network
-  end
+
 end
--- db/migrate/008_add_name_and_genres_to_shows.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndGenreToShows < ActiveRecord::Migration
-  def change
-    add_column :shows, :name, :string
-    add_column :shows, :genre, :string
-  end
-end
