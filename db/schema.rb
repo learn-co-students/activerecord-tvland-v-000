bf02a2d49bfa727be01476c9791e8a6ea34a10bb
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-17 03:48:13 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 9) do
   create_table "shows", force: :cascade do |t|
     t.string  "name"
     t.integer "network_id"
-    t.string  "genres"
+    t.string  "genre"
     t.string  "day"
     t.string  "season"
   end
