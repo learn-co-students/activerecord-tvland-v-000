4d6880664ccd8bb5677d9ff245a18a8225dafd33
arturusfury <bri.reynolds@gmail.com>
2016-07-26 21:56:10 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
+# Actor Class
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,5 @@
+# Show Class
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
 end
