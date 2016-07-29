405f9e92a7ec99d4154ebc9b700c37f37f6ae827
Jennifer Pazos <jmp521@gmail.com>
2016-07-29 06:07:56 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
   
+  # Show.create(:name => "Community", :day => "Thursday", :season => "Winter")
 end
