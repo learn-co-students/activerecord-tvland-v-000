7fd410c9b356b01e2fe4c03729f059312f7e7635
Eric Chu <erickentleongchu@gmail.com>
2016-08-15 23:51:47 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.characters.map do |char|
+      "#{char.name} - #{char.show.name}"
+    end
+  end
 end
