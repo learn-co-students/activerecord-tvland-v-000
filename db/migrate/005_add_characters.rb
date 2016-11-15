29be996040b0af68c81cb27f0635d3073f2ed3f3
William Story <wstory@villanova.edu>
2016-11-15 02:08:42 +0000

    Done.

---
+++ db/migrate/005_add_characters.rb
@@ -0,0 +1,10 @@
+class AddCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :catchphrase
+      t.integer :show_id
+      t.integer :actor_id
+    end
+  end
+end
