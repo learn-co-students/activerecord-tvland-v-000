6bd30174b5fa9fcf8bd2080c223cd008929f15e0
bharti gurnaney <bharti0717@yahoo.com>
2016-11-17 21:56:24 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+   has_many :characters
+   has_many :shows, through: :characters
+
+   def full_name
+   self.first_name + " " + self.last_name
+   end
+
+   def list_roles
+
+   self.characters.collect do |character|
+    character.name + " - " + Show.find(character.show_id).name
+   end
+
+   end
+
 end
