9f33b6034dde039f60bdb707cfc959ed5d1fca19
Ray Mulligan <rmullig2@optonline.net>
2016-11-19 22:04:32 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
 
   has_many :characters
   has_many :actors, through: :characters
-  has_one :network
+  belongs_to :network
 
   def build_network(attr = {})
     self.network = Network.create(attr)
