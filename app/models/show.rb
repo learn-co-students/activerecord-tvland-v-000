517d79101bc58c598d93bdabe09a2ac321b66c1c
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-12-08 05:15:13 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(letters)
+    self.network = Network.new(letters)
+  end
 end
