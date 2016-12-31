fdeff9030281bf250a555ebdd0b74ae934e38393
Umohm1 <umohm12@gmail.com>
2016-12-31 00:27:49 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :show
+    
+    def say_that_thing_you_say
+      "#{self.name} always says: #{self.catchphrase}"
+    end 
 end
+
+#character.show => #<Show>
+#character.actor => #
+#actor.characters => [#<Char..>,#<Char...>]
+#show.actors => [#<Actors>,#<Actors>]
+
+#character.show.actors
---
+++ db/migrate/20161230235320_addcatchphrasetocharacters.rb
@@ -0,0 +1,5 @@
+class Addcatchphrasetocharacters < ActiveRecord::Migration
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+end
