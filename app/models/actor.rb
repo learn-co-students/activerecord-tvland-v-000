f8431ba80b37a3ad5af4d069cb9e8d635d2ed0e6
HANA LEANDER <hana0070@gmail.com>
2017-01-13 03:52:08 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  #that lists all of the characters that actor has.
+
+  def list_roles
+    self.collect {|character| "#{character_name} - #{show_name}"}
+  end
+
 end
