0069274c8c31d8712b134e1659c28a1ced1260fd
vanita eckert <vanita112003@gmail.com>
2017-02-05 18:12:06 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+      [self.first_name, self.last_name].join(" ")
+    end
+    
+
+    def list_roles
+      name = self.characters.map do |each|
+        each.name
+      end
+      show = self.shows.map do |each|
+        each.name
+      end
+         (name + show).join(" - ")
+    end
 end
