a40eb4e6e23649e1759252746fece1363664f2db
bentonwong <bentonwong@gmail.com>
2017-03-11 20:21:32 -0800

    migrated title and added class methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  has_many :networks, through: :shows
+
+  def full_name
+    [self.first_name, self.last_name].join(' ')
+  end
+
+  def list_roles
+    self.characters.collect {|role| role.name}
+  end
+
 end
