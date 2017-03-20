53facf0ee639e04562db4e1e840095f1f5aa487a
Efrain Perez Jr <efrainperezjr@live.com>
2017-03-20 07:47:59 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(args={})
+    self.network = Network.new(args)
+  end
 end
