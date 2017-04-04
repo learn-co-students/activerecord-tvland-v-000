955cc146b6473978be9b2b301e3233ed4a752477
Parv Kumar <pkumar@trinity.edu>
2017-04-04 18:29:36 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{first_name} #{last_name}"
+
+  end
+  def list_roles
+    characters = self.characters
+    array = []
+    characters.each do |character|
+      array<< "#{character.name} - #{character.show.name}"
+    end
+    array
+
+  end
+
 end
