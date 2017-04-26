c21c225f52ba212e7e4cc9f8f8e97e5bd66aac4e
Zenia Villa <zeniavilla@hotmail.com>
2017-04-26 13:55:41 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(call_letters_hash)
+   self.network = Network.create(call_letters_hash)    
+  end
+
 end
