20b40b13c5703148e1ff25caa57df23444608eb4
zszalas2 <zszalas@gmail.com>
2017-04-29 08:37:01 -0400

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
+	has_many :characters
+	has_many :actors, through: :characters
+	belongs_to :network
+
+	def build_network(call_letter_hash)
+		self.network = Network.create(call_letter_hash)
+	end
   
 end
