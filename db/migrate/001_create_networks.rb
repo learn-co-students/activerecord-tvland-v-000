2c0823235e29f569b63d42ce6f52c76f4c0ba693
Feiran Wang <wangfeiranpku@163.com>
2017-05-02 06:11:06 +0000

    Done.

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -6,7 +6,8 @@
 class CreateNetworks < ActiveRecord::Migration
   #define a change method in which to do the migration
   def change
-    create_table :networks do |t| #we get a block variable here for the table
+    create_table :networks do |t|
+      #we get a block variable here for the table
       #primary key of :id is created for us!
       # defining columns is as simple as t.[datatype] :column
       t.string :call_letters
---
+++ db/migrate/006_add_actor_id.rb
@@ -0,0 +1,5 @@
+class AddActorId < ActiveRecord::Migration
+  def change
+    add_column :characters, :actor_id, :integer
+  end
+end
