ea9a69fa34e2ba4678c2ee9e0883bc38527a1a9d
WilliamBarela <william.barela.dev@gmail.com>
2017-05-14 12:29:48 +0900

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def build_show(name:)
+    self.name = name
+  end
 end
