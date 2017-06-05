1c67d001fe7cc43afc181a6585e5abcfc3317056
curlywallst <nnoyes@comcast.net>
2017-06-05 12:12:32 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters.all.collect {|char| "#{char.name} - #{char.show.name}" if char.actor_id == self.id}
+  end
+
+
+
+
 end
