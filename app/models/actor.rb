7d6796dafc809fed33ca53ddab3e489033db28c2
Sam Zeidman <sam.zeidman@gmail.com>
2017-06-05 21:56:21 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+  # TODO: build a method on actor that will return an array of
+  # strings in the form "#{character_name} - #{show_name}"
+    characters.collect {|c| "#{c.name} - #{c.show.name}"}
+  end
+
 end
