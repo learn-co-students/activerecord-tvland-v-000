ff48f3e2a2444f67157337fd5b6bd9454bedb565
jessica fletcher <cara.kane@gmail.com>
2017-06-06 19:19:45 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,28 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #this is the official solution, which I've added in here for reference
+    # characters.collect do |character|
+    #   "#{character.name} - #{character.show.name}"
+    # end
+
+    a = nil
+    b = nil
+    c = []
+    self.characters.each do |c|
+      a ="#{c.name}"
+    end
+
+    self.shows.each do |s|
+      b ="#{s.name}"
+    end
+
+    c << "#{a} - #{b}"
+  end
 end
