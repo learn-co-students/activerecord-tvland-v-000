e5b7b8f1dab25096ab86f18a883e92ac0d4d4fdb
Aimee Carney <acarney29@gmail.com>
2017-06-14 00:51:47 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,15 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
+  def list_roles
+    self.characters.collect {|character| character.name}    
+  end
 
 end
