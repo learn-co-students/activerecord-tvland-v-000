a44c6699edd43b57b239dc12a8bb624c0123a0ec
sarjumulmi <mulmi.sarju@gmail.com>
2017-06-15 13:42:04 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(call_letters:)
+    self.network = Network.new(call_letters: call_letters)
+    self.network
+  end
+
 end
---
+++ db/migrate/009_add_column_genre_to_show.rb
@@ -0,0 +1,6 @@
+class AddColumnGenreToShow < ActiveRecord::Migration
+  def change
+    add_column :shows, :genre, :string
+
+  end
+end
