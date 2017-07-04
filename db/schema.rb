20bfaba896cf6158bc8358494f35cfbee710b4b5
charlie lee <ctlee9096@gmail.com>
2017-07-04 16:36:59 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 5) do
     t.string  "name"
     t.integer "show_id"
     t.integer "actor_id"
+    t.string  "catchphrase"
     t.index ["actor_id"], name: "index_characters_on_actor_id"
     t.index ["show_id"], name: "index_characters_on_show_id"
   end
@@ -33,6 +34,9 @@ ActiveRecord::Schema.define(version: 5) do
   create_table "shows", force: :cascade do |t|
     t.string  "name"
     t.integer "network_id"
+    t.string  "genre"
+    t.string  "day"
+    t.string  "season"
   end
 
 end
