dfb9e16948c2bd6e803733873f4126e386abf5c5
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-07-07 22:14:23 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+
+  def self.build_network
+    self.network.call_letters
+  end
+
 end
