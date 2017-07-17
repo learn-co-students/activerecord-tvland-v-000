768abbed635b486ac67e849afd627630b98a367b
Catherine Cordova <ccordova41@gmail.com>
2017-07-17 15:37:25 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #lists all of the characters that actor has.
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+  end
 end
+end 
