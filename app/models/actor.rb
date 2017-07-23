7cb137968f4091346d0439562d4a164cb11d7413
alexisadorn <alexisadorn@gmail.com>
2017-07-23 16:33:18 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,11 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.name - self.characters.shows
+    roles = []
+    array_of_characters = self.characters.map {|character| character.name}
+    array_of_shows = self.shows.map {|shows| shows.name}
+    role = array_of_characters.concat array_of_shows
+    roles << role.join(" - ")
+    roles
   end
 end
