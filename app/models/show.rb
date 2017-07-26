1a96bf8ec5c04c1062143bf5e22d2432a716a7a7
Seb Gertz <sebgertz@gmail.com>
2017-07-26 22:15:22 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,6 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def build_network(call_letters)
-    network = Network.find_or_create_by(call_letters)
-    self.new.call_letters = call_letters
+    self.network = Network.find_or_create_by(call_letters)
   end
 end
