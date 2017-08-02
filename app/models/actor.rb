13c8442b1122562ec05f5cebdbfb385f38f53b74
Enoch2k2 <enoch2k2@hotmail.com>
2017-08-02 11:55:40 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    shows.collect{|show| "#{show.characters.detect{|character| character.actor.full_name == self.full_name}.name} - #{show.name}"}
+  end
 end
