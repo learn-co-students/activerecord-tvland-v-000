06decd393d00246c7e4c8d7bb2260513eb95697b
zoebisch <zoebisch@gmail.com>
2017-08-08 21:28:48 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    chars = Character.find_by_id(self.id)
+    chars.name + " - " + Show.find_by_id(chars.show_id).name
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(hash)
+    self.network = Network.create(hash)
+  end
+
 end
