c42414e8f99c25e7d67626fc23b2697529a1ee02
Tyler <tbuchhei@alumni.nd.edu>
2017-08-21 13:01:10 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(info)
+    self.network = Network.new(info)
+  end
 end
