d84fa17768990e0ff4584daf0433ec6736393545
github username <github email address>
2017-09-10 00:27:23 -0400

    Done.

---
+++ db/migrate/003_add_character_id_to_shows.rb
@@ -0,0 +1,5 @@
+class AddCharacterIdToShows < ActiveRecord::Migration
+  def change
+    add_column :shows, :character_id, :integer
+  end
+end
---
+++ db/migrate/007_add_character_id_to_actors.rb
@@ -0,0 +1,5 @@
+class AddCharacterIdToActors < ActiveRecord::Migration
+  def change
+    add_column :actors, :character_id, :integer
+  end
+end
