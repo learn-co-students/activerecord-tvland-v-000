54ea7d12192842efbc62e513a3ac254385173fa9
Joanna Sese <jmgsese@gmail.com>
2017-09-15 16:18:04 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ActiveRecord::Base
-  has_many :actors
-  has_many :characters, through: :actors
+  has_many :characters
+  has_many :actors, through: :characters
   belongs_to :network
 end
