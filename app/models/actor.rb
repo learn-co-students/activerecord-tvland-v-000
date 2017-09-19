4d13ac7a826804aeaa911d13c6842118a686bf66
Elvin <Dimix181@Gmail.com>
2017-09-18 22:53:30 -0400

    update list_roles for actor class

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,9 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-   "#{self.characters.first.name} - #{self.shows.first.name}"
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 
 end
