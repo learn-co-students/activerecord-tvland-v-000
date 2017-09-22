9171887d10b13ce0a8924ea916410ef7a5fd91c6
Sam Marshall <samuel.r.marshall@gmail.com>
2017-09-21 22:25:35 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first_name+" "+last_name
+  end
+
+  def list_roles
+    roles = []
+    characters.each {|c| roles << c.name+" - "+c.show.name}
+    roles
+  end
+
 end
---
+++ db/migrate/20170922013006_create_characters.rb
@@ -0,0 +1,8 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :catchphrase
+    end
+  end
+end
