daa8ca2973c7e79d345df10ec6f81a96577cc2f7
ryanwevans <ryanwevans@icloud.com>
2017-09-25 22:45:37 -0700

    added db migrations, association methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  has_one :network, through: :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def self.build_show(show)
+    show.name
+  end
 end
