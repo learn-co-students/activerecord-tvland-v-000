685999f429b0d37bd726ffa85ae66496db85d9c3
Tavish Harrison <creationmargin26@gmail.com>
2017-09-26 23:45:39 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    x = []
+    self.characters.each do |t|
+      x << t.name + " - " + t.show.name
+    end
+    x
+  end
 end
