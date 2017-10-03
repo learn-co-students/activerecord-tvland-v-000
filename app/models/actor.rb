812cce41baf2a837c6ec83b7e1881c2f45657ce6
Alexander Friedland <friedlandaj@gmail.com>
2017-10-03 01:01:26 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  #Associate the Actor model with the Character and Show model.
+  #An actor should have many characters and many shows through characters.
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    puts self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    #expect(emilia.list_roles).to include("Khaleesi - Game of Thrones")
+    roles = Array.new
+    self.characters.each do |character|
+      roles << "#{character.name} - #{character.show.name}"
+    end
+    roles
+  end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,5 +3,6 @@ class CreateCharacters < ActiveRecord::Migration
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
+      t.integer :actor_id
   end
 end
