ec8c62dcf2f84bd4a99529afe048b777abeabcb9
Mary Benitez <meebenitez@gmail.com>
2017-10-11 02:10:17 +0000

    Done.

---
+++ db/migrate/004_add_first_last_names.rb
@@ -0,0 +1,9 @@
+class AddFirstLastNames < ActiveRecord::Migration
+  def change
+    create_table :actors do |t|
+      #primary key of :id is created for us!
+      t.string :first_name
+      t.string :last_name
+    end
+  end
+end
