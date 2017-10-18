2ab58e39b22fdc3141364762db908f207b50f878
Richard Steinbach <steinbach.dick@gmail.com>
2017-10-17 20:41:54 -0700

    AR tvland

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+	has_many :characters
+	has_many :shows, through: :characters
+	@characters = []
+	@shows = []
+
+	def full_name
+		full = "#{self.first_name} #{self.last_name}"
+	end
+
+	def list_roles
+		n = ''
+		s = ''
+		role_arr =[]
+		self.characters.collect do |character| n = character.name
+			self.shows.collect {|show| s = show.name}
+			role_arr << "#{n} - #{s}"
+		end
+		role_arr
+	end
 end
