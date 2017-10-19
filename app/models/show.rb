88e686e2367f85a3e8c8207491c5f7faf3210118
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-19 19:16:52 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(hash)
+    self.network = Network.new(hash)
+  end
 end
