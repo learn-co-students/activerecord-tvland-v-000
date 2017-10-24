f0bb3041b49858202905c53fa2336c3f3c73aa8d
github username <github email address>
2017-10-24 13:16:41 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles #bad
+    characters = self.characters.collect { |character| character.name}
+    shows = self.shows.collect { |show| show.name}
+    roles = "#{characters[0]} - #{shows[0]}"
+  end
 end
---
+++ db/migrate/007_add_characters_to_show.rb
@@ -0,0 +1,8 @@
+class AddCharactersToShow < ActiveRecord::Migration
+  #to make an additive change to a schema we create
+  #a new migration, and then in the change method,
+  #we make the change.
+  def change
+    add_column :characters, :show_id, :integer
+  end
+end
