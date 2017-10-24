7a66bdd15255301940e0740cd553f480a79bdfdc
github username <github email address>
2017-10-24 13:27:13 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,12 +3,20 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name + " " + self.last_name
+    # self.first_name + " " + self.last_name
+    #better
+    "#{self.first_name} #{self.last_name}"
   end
 
-  def list_roles #bad
-    characters = self.characters.collect { |character| character.name}
-    shows = self.shows.collect { |show| show.name}
-    roles = "#{characters[0]} - #{shows[0]}"
+  def list_roles
+    #bad
+    # characters = self.characters.collect { |character| character.name}
+    # shows = self.shows.collect { |show| show.name}
+    # roles = "#{characters[0]} - #{shows[0]}"
+
+    #better
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 end
