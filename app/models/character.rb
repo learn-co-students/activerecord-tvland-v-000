756a431caf71d38ab969a578db383a5fc6c9e8bc
John Tuttle <thejwt@gmail.com>
2017-10-24 18:45:42 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,20 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def actor=(actor)
+    @actor = actor
+    actor.characters << self
+  end
+  #binding.pry
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def method_name
+
+  end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network(call_letters:)
+    network = Network.new(call_letters: call_letters)
+    network.shows << self
+  end
+
 end
