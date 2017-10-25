d45d05af5adb170c83be9073c5e75873980b3bd1
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-10-25 15:43:54 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.all.collect {|s| "#{s.name} - #{s.show.name}"}
+  end
+  # An actor has many characters and has many shows through characters.
+  # A character belongs to an actor and belongs to a show.
+  # A show has many characters and has many actors through characters.
 end
