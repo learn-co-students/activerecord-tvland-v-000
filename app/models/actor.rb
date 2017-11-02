ed96aa0cc8b08faf8ff7e3d034c6949f61a4d0fa
alpbourne <alpbourne@gmail.com>
2017-11-02 15:38:01 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,6 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.map do {|character| "#{character.name} - #{character.show.name}"}
-    end
+    characters.collect {|character| "#{character.name} - #{character.show.name}"}
   end
 end
