bb414c7e5aa3e51f496006ac78227e8fa158f008
Justin Matos <matos89@gmail.com>
2017-11-11 13:01:19 -0800

    Done.

---
+++ db/migrate/20171111011453_update_actors.rb
@@ -0,0 +1,5 @@
+class UpdateActors < ActiveRecord::Migration
+  def change
+    add_column :characters, :actor_id, :integer
+  end
+end
---
+++ db/migrate/20171111210021_update_shows_with_genre.rb
@@ -0,0 +1,5 @@
+class UpdateShowsWithGenre < ActiveRecord::Migration
+  def change
+    add_column :shows, :genre, :string
+  end
+end
