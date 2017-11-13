42cdac3029f63d1f82d47a856e8ccf6cabe2e98f
Yujun Sung <yujunsung@gmail.com>
2017-11-13 04:37:04 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,26 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+
+    "#{self.first_name} #{self.last_name}"
+
+  end
+  def list_roles
+    list = []
+    counter = 0
+    self.characters.each do |character|
+      #binding.pry
+      show = Show.find(character.show_id)
+      if show != nil
+        list[counter] = "#{character.name} - #{show.name}"
+        counter += 1
+      end
+
+      #binding.pry
+    end
+    list
+  end
+
 end
---
+++ db/migrate/005_create_charactors.rb
@@ -0,0 +1,9 @@
+class CreateCharactors < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      #primary key of :id is created for us!
+      t.string :name
+      t.integer :show_id
+    end
+  end
+end
