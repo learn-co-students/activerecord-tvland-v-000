544c23713ae83a2b713678630ec436db7ba37330
Adam Fudge <coolfudge@gmail.com>
2017-11-15 16:48:03 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string  "name"
+    t.integer "show_id"
+    t.string  "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -25,6 +31,7 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "shows", force: :cascade do |t|
     t.string  "name"
     t.integer "network_id"
+    t.string  "genre"
   end
 
 end
