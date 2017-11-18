37824a543ef9f5037e19543f44a68f75a5283554
Oski <github email address>
2017-11-18 11:59:50 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+
+
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -27,6 +27,7 @@ describe Character do
     urkel = Character.new(:name => "Steve Urkel")
     urkel.catchphrase = "Did I do that?"
     urkel.save
+
     expect(Character.find_by(:id => urkel.id).catchphrase).to eq(urkel.catchphrase)
 
     expect(urkel.say_that_thing_you_say).to eq("#{urkel.name} always says: #{urkel.catchphrase}")
