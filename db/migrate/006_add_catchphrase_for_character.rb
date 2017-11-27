9b3d5d4dceabf23fc6cbeede53f452a8b73bd5ec
Carley Tripp <carley.tripp@outlook.com>
2017-11-27 04:53:02 +0000

    Done.

---
+++ db/migrate/006_add_catchphrase_for_character.rb
@@ -0,0 +1,5 @@
+class AddCatchphraseForCharacter < ActiveRecord::Migration[4.2]
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+end
---
+++ db/migrate/007_add_genre_to_show.rb
@@ -0,0 +1,5 @@
+class AddGenreToShow < ActiveRecord::Migration[4.2]
+  def change
+    add_column :shows, :genre, :integer
+  end
+end
