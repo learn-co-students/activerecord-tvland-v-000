47f73384e20d11a621642a1c174dd0e171127188
Mel Rudick <melissarudick@MacBook-Air.local>
2017-11-27 18:18:12 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect {|a| "#{a.name} - #{a.show.name}"}
+  end # doesn't this need to be self?
 end
