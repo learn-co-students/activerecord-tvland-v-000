9620ff8eaea4ccb3f004da4a318d490edaa1a475
github username <github email address>
2017-11-28 09:49:57 +0200

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each_with_index {|s, i| roles[i] = s.name}
+    self.shows.each_with_index {|s,i| roles[i] += " - #{s.name}"}
+    roles
+  end
+
 end
