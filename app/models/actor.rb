8cda41402c9af92fb2e0b62f55be6cfffc4038f3
roitblatari <roitblatari@gmail.com>
2017-12-05 10:01:53 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+     "#{self.first_name} #{self.last_name}"   
+  end
+
+  def list_roles
+    binding.pry
+    "#{self.characters.name} - #{self} "
+  end
 end
+
+
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
 end
