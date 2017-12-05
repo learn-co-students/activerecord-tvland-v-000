75bfa59242a3c2bad3b99666656142fcc651f384
roitblatari <roitblatari@gmail.com>
2017-12-05 12:56:10 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,13 +1,17 @@
 class Actor < ActiveRecord::Base
   has_many :characters
+  
 
   def full_name
      "#{self.first_name} #{self.last_name}"   
   end
 
   def list_roles
-    binding.pry
-    "#{self.characters.name} - #{self} "
+    self.characters.map do |c|
+      "#{c.name} - #{c.show.name}"
+      # binding.pry
+    end
+    # "#{self.characters.name} - #{self.characters.show} "
   end
 end
 
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
   
+  # def build_network(nw)
+  #   nw[:call_letters]
+   
+  # end
 end
