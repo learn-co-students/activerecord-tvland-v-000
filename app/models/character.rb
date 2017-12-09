dd907891e0aef535a9d8450a448fdef85e65c85c
Susan Fowler <infopro94@gmail.com>
2017-12-09 00:43:18 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actors
+  belongs_to :shows
+
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :show_id
+    end
+  end
+
 end
