288b3ddf5eba25883d730f55418de8f433560a5d
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-09 02:18:53 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(hash)
+    network = Network.new(hash)
+    #network.shows << self
+    self.network = network
+  end
+
 end
