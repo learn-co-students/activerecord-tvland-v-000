41bc5b95a72a178df3107c125c1a82a45da0e511
Susan Fowler <infopro94@gmail.com>
2017-12-09 02:33:25 +0000

    Done.

--- db/migrate/08_add_catchphrase_to_character.rb
+++ db/migrate/08_add_catchphrase_to_character.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
+class AddColumnCatchphrase < ActiveRecord::Migration[4.2]
 
   def change
     add_column :characters, :catchphrase, :string
