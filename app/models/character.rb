f156484bcdd910fb31794022ac10d3fd605ea485
Matthew Hunger <matt.hunger@gmail.com>
2017-12-13 18:19:50 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def says_that_thing_you_say
+    binding.pry
+  end
+
 end
---
+++ db/migrate/009_create_shows_table.rb
@@ -0,0 +1,8 @@
+class CreateShowsTable < ActiveRecord::Migration[4.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :genre
+    end
+  end
+end
