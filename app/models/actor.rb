b0d3b2f5a8674cfb3abee4fda82cb30af6c69f55
Kaitlin Kelly <kk504408@gmail.com>
2017-12-21 04:27:24 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def full_name
+    [self.first_name, self.last_name].join(" ")
+  end
+
+  def list_roles
+    self.characters.map {|c|  [c.name, c.show.name].join(" - ") }
+  end
+
 end
