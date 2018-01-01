5b6b34f5e1ed808e44ecb3ba07dd8df620539b59
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 12:19:33 +0000

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :catchphrase
+      t.integer :actor_id
+      t.integer :show_id
+    end
+  end
+end
