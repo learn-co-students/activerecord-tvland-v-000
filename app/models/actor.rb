24a640076d3cb29ae747d1a48c569c679d71b552
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 12:22:15 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.map do |character|
+    characters.collect do |character|
       "#{character.name} - #{character.show.name}"
     end
   end
