924e08f59b7d800993dafc8923a641966be1699e
Hannah Carnes <hecarnes@gmail.com>
2018-01-02 20:44:50 -0500

    done.

--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -6,6 +6,8 @@ class CreateShows < ActiveRecord::Migration[4.2]
     create_table :shows do |t|
       #primary key of :id is created for us!
       t.string :name
+      t.string :day
+      t.string :season
     end
   end
 end
