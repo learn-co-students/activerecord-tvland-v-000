08401a569312675a5f9303651c6f1854f477cce8
ronald vilorio <soccer23415@gmail.com>
2018-01-13 05:25:47 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+
+  def build_network(attributes)
+    new_network = Network.new(attributes)
+    self.network = new_network
+  end
   
 end
