a56ec6a92246fcb148dbb0bbc67347c4a8fd7691
lizbur10 <liz.burton147@gmail.com>
2018-01-14 12:51:56 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(call_letters)
+    self.network = Network.find_or_create_by(call_letters)
+  end
 end
