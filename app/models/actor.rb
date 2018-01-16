3d615694f9df582baa792681397af7ebbea5931d
Sally Zasloff <szasloff@gmail.com>
2018-01-16 23:55:45 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,6 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
@@ -12,5 +11,4 @@ class Actor < ActiveRecord::Base
       "#{character.name} - #{character.show.name}"
     end
   end
-
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -5,5 +5,4 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-
 end
