177e1817ddad20c939623469913a98f48f2543dc
Nolan Hughes <nhughes987@gmail.com>
2018-01-17 18:55:10 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    show = shows.collect { |show| show.name }
+    character = characters.collect { |character| character.name }
+    "#{character[0]} - #{show[0]}"
+  end
 end
