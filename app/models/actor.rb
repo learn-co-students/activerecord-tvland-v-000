b777c2ac0c3d28295653b0a907917b1ff5b21230
churchwerks <steven.lennis@icloud.com>
2018-01-18 10:48:43 -0600

    3rd Commit
    Created Characters Table

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,11 @@
 class Actor < ActiveRecord::Base
-  
+
 end
+
+# Actor
+#   has a first and last name (FAILED - 1)
+#   has associated characters in an array (FAILED - 2)
+#   can build its associated characters (FAILED - 3)
+#   can build its associated shows through its characters (FAILED - 4)
+#   can list its full name (FAILED - 5)
+#   can list all of its shows and characters (FAILED - 6)
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,8 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |t|
+      #primary key of :id is created for us!
+      t.string :name
+    end
+  end
+end
