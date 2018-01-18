8c36edac9e5e4f2b37e2719b8bdc83ad4bb2f42c
churchwerks <steven.lennis@icloud.com>
2018-01-18 16:30:26 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,13 +2,4 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   belongs_to :network
-
-  # def build_network(show)
-  #   binding.pry
-  #   self
-  # end
-  #
-  # def network_call_letters(show)
-  #   #binding.pry
-  # end
 end
