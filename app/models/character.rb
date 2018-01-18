8ad42d3b5a6ec04959365754294d7732c6af37f6
churchwerks <steven.lennis@icloud.com>
2018-01-18 16:29:17 -0600

    Final
    All tests passing

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,15 +6,9 @@ class Character < ActiveRecord::Base
     "#{self.name} always says: #{self.catchphrase}"
   end
 
+  def network
+    #binding.pry
+  end
 
 
 end
-# can chain-build associations to which it belongs (FAILED - 1)
-#
-# Failures:
-#
-# 1) Character can chain-build associations to which it belongs
-#    Failure/Error: malcolm.build_show(:name => "Firefly").build_network(:call_letters => "Fox")
-#
-#    NoMethodError:
-#      undefined method `build_network' for #<Show id: nil, name: "Firefly", network_id: nil>
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,14 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+  belongs_to :network
 
-  def build_network(show)
-
-  end
-
-  def network_call_letters
-
-  end
+  # def build_network(show)
+  #   binding.pry
+  #   self
+  # end
+  #
+  # def network_call_letters(show)
+  #   #binding.pry
+  # end
 end
