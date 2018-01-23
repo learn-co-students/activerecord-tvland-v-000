20137a14cc6b055efde880f07e2d72414dfda3bd
Brad Hambrice <hambrice@uab.edu>
2018-01-23 02:36:35 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(hash)
+    self.network = Network.find_or_create_by(hash)
+  end
 end
---
+++ db/migrate/009_add_times_to_shows.rb
@@ -0,0 +1,6 @@
+class AddTimesToShows < ActiveRecord::Migration
+  def change
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+  end
+end
