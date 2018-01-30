27a0858f0139dabae11b08313f7b8d4406e3ffea
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 19:03:28 +0000

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,8 @@
 class Network < ActiveRecord::Base
   has_many :shows
+  has_many :characters, through: :shows
 
   def sorry
-    "We're sorry about passing on John Mulaney's pilot"
-  end
+      "We're sorry about passing on John Mulaney's pilot"
+    end
 end
