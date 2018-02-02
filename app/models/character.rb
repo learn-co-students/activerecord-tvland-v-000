7ecaa0ced2fe4445f9f70165b329c930f98d380f
Andy Maskiell <maskiella@gmail.com>
2018-02-02 23:02:54 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(show_hash)
+    show = Show.create(show_hash)
+    self.show_id = show.id
+    self.show
+  end
 end
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :catchphrase
+      t.integer :actor_id
+      t.integer :show_id
+    end
+  end
+end
