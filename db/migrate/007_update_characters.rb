ccc8670cbc37f7e0a79ec401e707f965d3c0cbbf
funemployeddad <funemployeddad@gmail.com>
2018-02-07 15:52:34 -0700

    Done.

---
+++ db/migrate/007_update_characters.rb
@@ -0,0 +1,8 @@
+class UpdateCharacters < ActiveRecord::Migration[4.2]
+
+  def change
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :catchphrase, :string
+  end
+
+end
