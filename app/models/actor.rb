5bcd35823e3f64443c5e2e520d7e1bdc6e047ac1
jljardon <github email address>
2018-02-20 23:17:05 -0500

    add association methods for all models

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+   has_many :characters
+   has_many :shows, through: :characters
+
+   def full_name
+      first_name + ' ' + last_name
+   end
+
+   def list_roles
+      characters.map do |characters|
+         characters.name + ' - ' + characters.show.name
+      end.join('')
+   end
 end
