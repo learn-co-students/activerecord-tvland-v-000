c441f6228537c7996a064d5efe91c06b93b008e2
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-22 06:30:43 -0500

    Add migrations, associations, and methods to actor, character, and show

--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -1,5 +1,5 @@
-class AddShowsToNetwork < ActiveRecord::Migration
-  #to make an additive change to a schema we create
+class AddShowsToNetwork < ActiveRecord::Migration[4.2]
+  #to make an additive change to a schema, we create
   #a new migration, and then in the change method,
   #we make the change.
   def change
