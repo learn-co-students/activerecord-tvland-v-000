e895799013677e23dc25cb1c5b9fb73bfd51b46d
Katrina Brinson <kbrinso@gmail.com>
2018-02-24 01:15:17 +0000

    20 examples 4 failures

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
-  attr_accessor :catchphrase
+  # attr_accessor :catchphrase
 
   def say_that_thing_you_say
     return "#{self.name} always says: #{self.catchphrase}"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 9) do
+ActiveRecord::Schema.define(version: 10) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 9) do
     t.string "name"
     t.integer "actor_id"
     t.integer "show_id"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
