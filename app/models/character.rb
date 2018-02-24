2db41907f2c9bd026f3324e90ae1323941507d51
Katrina Brinson <kbrinso@gmail.com>
2018-02-24 01:00:39 +0000

    stuck on catchphrase

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,22 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
+
+  attr_accessor :catchphrase
+
+  def say_that_thing_you_say
+    return "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  # def catchphrase
+  #   # self.add_column
+  #   # binding.pry
+  # end
+  # urkel = Character.new(:name => "Steve Urkel")
+  # urkel.catchphrase = "Did I do that?"
+  # urkel.save
+  # expect(Character.find_by(:id => urkel.id).catchphrase).to eq(urkel.catchphrase)
+  #
+  # expect(urkel.say_that_thing_you_say).to eq("#{urkel.name} always says: #{urkel.catchphrase}")
+
 end
