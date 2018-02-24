5889d12772e5ef18b00babe2911cb89a80f3ed25
Katrina Brinson <kbrinso@gmail.com>
2018-02-24 00:22:59 +0000

    all migration relationships completed

---
+++ db/migrate/009_add_actors_to_shows.rb
@@ -0,0 +1,5 @@
+class AddActorsToShows < ActiveRecord::Migration[4.2]
+  def change
+    add_column :actors, :show_id,  :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 9) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "show_id"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "actor_id"
+    t.integer "show_id"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
@@ -20,6 +32,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.integer "actor_id"
   end
 
 end
