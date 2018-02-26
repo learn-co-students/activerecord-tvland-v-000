f84b0fdb75aa102597deffa9885e1755d51fa200
Araba Taylor <araba.taylor20@gmail.com>
2018-02-26 04:21:33 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,10 @@ class Actor < ActiveRecord::Base
   def self.full_name
     self.first_name + self.last_name
   end
+
+  def self.list_roles
+    self.characters do |c|
+      puts "c - c.show"
+    end
+  end
 end
