edc740283efec728b1137bca8c3ee8325d0313bf
Araba Taylor <araba.taylor20@gmail.com>
2018-02-26 04:09:15 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,8 +1,8 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_many :shows, :through :characters
+  has_many :shows, through: :characters
 
   def self.full_name
-    
+    self.first_name + self.last_name
   end
 end
