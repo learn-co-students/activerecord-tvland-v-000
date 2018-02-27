d1cf7ce67bac92c17398e8c537741ea50ae30355
Araba Taylor <araba.taylor20@gmail.com>
2018-02-27 03:23:20 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,13 +2,11 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  def self.full_name
+  def full_name
     self.first_name + self.last_name
   end
 
-  def self.list_roles
-    self.characters do |c|
-      puts "c - c.show"
-    end
+  def list_roles
+    self.characters
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
+  def build_show(name)
+
+  end
 end
