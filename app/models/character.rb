0dc05053661eafe407ab3868fd4bd7fa1a9e225c
Obafemi Fadairo <ofpupp@princeton.edu>
2018-03-03 04:42:18 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+
+  def say_that_thing_you_say
+    return self.name + " always says: " + self.catchphrase
+  end
+
+  def build_show(show)
+    new_show = Show.new(show)
+    self.show = new_show
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(network)
+    binding.pry
+    new_network = Network.new(network)
+    self.network = new_network
+  end
 end
