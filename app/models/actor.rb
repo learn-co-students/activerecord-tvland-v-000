2532c0630967085421803f7fd999cb82ac35394f
YanC Lin <yonalin1@gmail.com>
2018-03-07 23:23:18 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,9 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    characters.name
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 
 end
