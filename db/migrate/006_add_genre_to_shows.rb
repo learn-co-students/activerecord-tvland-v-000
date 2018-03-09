c3431be1e998c1da2d10b580440af69e4f619cf0
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-09 19:52:14 +0000

    Done.

---
+++ db/migrate/006_add_genre_to_shows.rb
@@ -0,0 +1,5 @@
+class AddGenreToShows < ActiveRecrod::Migration[4.2]
+  def change
+    add_column :shows, :genre, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "character_id"
   end
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
     t.string "catchphrase"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-    t.integer "character_id"
-    t.integer "show_id"
+    t.string "actor_id"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -37,6 +31,7 @@ ActiveRecord::Schema.define(version: 8) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
+    t.string "genre"
     t.integer "network_id"
   end
 
