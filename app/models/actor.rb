650e63dd0f0ce2d6533a41a3f960d1425c587ee7
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-15 23:41:48 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :character
+  has_many :show, through: :character
+
+  def full_name
+  "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.collect do |actor|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
