4c815efd9a5de35de26e5d0245a01000e8247a07
Aveline <amouraveline@gmail.com>
2018-03-22 18:10:10 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,9 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
+
 end
+
+
+# Show(id: integer, name: string, network_id: integer, genre: string, day: string, season: string)
