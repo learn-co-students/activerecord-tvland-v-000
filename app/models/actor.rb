a2151aa2b3b95a9e94304f30e4d7164a8b80a8eb
Jay Bednar <ironviralmedia@gmail.com>
2018-03-27 00:45:11 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    # binding.pry
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |c|
+      "#{c.name} - #{c.show.name}"
+    end
+  end
 end
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,8 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :show_id
+    end
+  end 
+end
