ab651d46f0ddc06f131f51ccd47831ed7f9a8731
Alex McKean <amckean@me.com>
2018-03-28 16:00:46 +0000

    Done.

---
+++ db/migrate/006_add_characters_to_actors.rb
@@ -0,0 +1,6 @@
+class AddCharactersToActors < ActiveRecord::Migration[5.1]
+  def change
+    add_column :characters, :actor_id, :integer
+  end
+
+end
---
+++ db/migrate/008_add_genre_to_tv_show.rb
@@ -0,0 +1,5 @@
+class AddGenreToTvShow < ActiveRecord::Migration[5.1]
+  def change
+    add_column :shows, :genre, :string
+  end
+end
