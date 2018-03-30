3e6f3869244c78ccada443ff65cc743a79743a38
Sam. O <samrey2018@gmail.com>
2018-03-30 22:11:07 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many  :characters
+  has_many  :shows, through: :characters
+
+
+ def full_name
+   # return the first_name and last_name of the actor
+   "#{ self.first_name } #{ self.last_name }"
+ end
+
+
+ def list_roles
+   # list_roles, that lists all of the characters that actor has.
+     self.characters.collect do |character|
+       "#{ character.name } - #{ character.show.name }"
+     end
+ end
+
+
 end
---
+++ db/migrate/009_add_season.rb
@@ -0,0 +1,8 @@
+class AddSeason < ActiveRecord::Migration[4.2]
+
+  def change
+    add_column :shows, :season, :string
+
+  end
+
+end
