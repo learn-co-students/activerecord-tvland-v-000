c77736bdcb8b38b18eb269944448481bfb1b3aed
Sam. O <samrey2018@gmail.com>
2018-03-30 22:50:10 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,14 +1,8 @@
 class Show < ActiveRecord::Base
     has_many   :characters
     has_many   :actors, through: :characters
-    has_one    :genre
     belongs_to :network
 
-    def genre
-         genre = Genre.create(genre: Dramedy)
 
-         self.genre = genre
-         self.genre
-    end
 
 end
