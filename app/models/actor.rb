9340f1b07858bab359efe2122f641559c8fbaca0
Frank McCallion <fkmccallion@gmail.com>
2018-04-03 02:44:40 +0000

    current update

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,14 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
+    self.first_name + " " + self.last_name
+    #{}"#{first_name} #{last_name}"
+  end
 
+  def list_roles
+    characters.collect do |c|
+      c.name + " - " + c.show.name
+      #"#{c.name} - #{c.show.name}"
+    end
   end
 end
