f1e3c7c12f39e6fad324f505043034f434a9a744
amanda johnson <manda.panda91@yahoo.com>
2018-04-11 22:19:29 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,8 @@
 class Character < ActiveRecord::Base
   belongs_to :show
-  belongs_to :actor, through: :show
-  
+  belongs_to :actor
+
   def say_that_thing_you_say
-    self.catchphrase  
+    "#{self.name} always says: #{self.catchphrase}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 6) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
+    t.integer "actor_id"
     t.string "catchphrase"
   end
 
@@ -30,8 +31,10 @@ ActiveRecord::Schema.define(version: 6) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
-    t.string "genre"
     t.integer "network_id"
+    t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
