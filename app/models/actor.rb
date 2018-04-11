90673c71d7a50aee37da81b87e0e84db951b2d2c
amanda johnson <manda.panda91@yahoo.com>
2018-04-11 16:47:15 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    self.select(:first_name, :last_name).join(" ")
+  end
+  
+  def list_roles
+    self.characters.name  
+  end
 end
