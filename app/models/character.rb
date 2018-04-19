3192baed18ce50d3ac641346a7c1d0ce6309c04e
Petya Djanovska <petya.djanovska@gmail.com>
2018-04-19 14:20:03 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(name)
+    self.show = Show.create(name)
+  end
+
+  def build_network(call_letters)
+    self.show.build_network(call_letters)
+  end
 end
