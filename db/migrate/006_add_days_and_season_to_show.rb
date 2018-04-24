78671f67d8f96e4a42c6ca3dd38fd0d92c357502
Trevor Seitz <trevinla@gmail.com>
2018-04-24 14:27:04 +0000

    automatically backed up by learn

--- db/migrate/006_add_days_and_season_to_show.rb
+++ db/migrate/006_add_days_and_season_to_show.rb
@@ -1,4 +1,4 @@
-class AddDaysAndSeasonToShow < ActiveRecord < Migration[4.2]
+class AddDaysAndSeasonToShow < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
     add_column :shows, :season, :string
