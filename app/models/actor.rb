8bd731ba1ff7944bdefbbeb6276f40c701e5e3df
Joanne Lee <vernapii@gmail.com>
2018-04-25 18:10:09 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :show, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
+    khaleesi = Character.new(:name => "Khaleesi")
+    khaleesi.actor = emilia
+    khaleesi.save
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :genre
+
+  def build_network(network)
+  end
 end
