76ca60b07fb8f344fe3e4729899b98f3df2a3bd2
Dee Race <deerace12@gmail.com>
2018-04-25 17:39:24 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,4 @@
 class Actor < ActiveRecord::Base
-
   has_many :characters
   has_many :shows, through: :characters
 
--- db/migrate/007_add_attributes_to_show.rb
+++ db/migrate/007_add_attributes_to_show.rb
@@ -1,4 +1,4 @@
-class AddDayAndGenreToShows < ActiveRecord::Migration[4.2]
+class AddAttributesToShow < ActiveRecord::Migration[4.2]
 
   def change
     add_column :shows, :genre, :string
