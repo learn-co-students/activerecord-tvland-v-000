665909599e243d0b160f621142f4f9724df9700f
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-30 02:47:27 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,9 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
+  has_many :actors, through: :characters
 
-  def build_network
-    network.name
-  end
+  # def build_network
+  #   network.name
+  # end
 end
