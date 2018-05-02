3c0b3dbdf8dfac8931c8b9b5e607b50b24360e9f
Vanessa Toro <vanetoro84@gmail.com>
2018-05-02 19:26:58 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,16 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    binding.pry
+    self.characters.map do |s|
+        "#{s.name}"
+    end
+  end
+
+
 end
