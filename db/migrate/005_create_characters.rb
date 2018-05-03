0c884da87bbeab9d25cd513302b2c96577fa39e4
Yina Zhu <yinagit@gmail.com>
2018-05-03 18:14:12 +0000

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,10 @@
-def CreateCharacters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[4.2]
 
   def change
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
+    end
+  end
+
 end
