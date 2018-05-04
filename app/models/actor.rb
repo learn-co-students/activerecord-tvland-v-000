27662cb49edd524516744d06b785208df298d0a2
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-04 23:25:19 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.map { |character| character.name }
+    characters.map { |character| "#{character.name} #{character.show.name}" }
   end
 
 end
---
+++ db/migrate/007_add_actor_and_show_to_character.rb
@@ -0,0 +1,6 @@
+class AddActorAndShowToCharacter < ActiveRecord::Migration[4.2]
+  def change
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :show_id, :integer
+  end
+end
