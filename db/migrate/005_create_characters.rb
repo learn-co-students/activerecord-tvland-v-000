7d6b0cd573fa718f511f8d95e3ea5cbc440fd3fb
Raiet Intraub <raietsaeed@gmail.com>
2018-05-06 02:25:39 +0000

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,6 +3,7 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
     create_table :characters do |c|
       c.string :name
       c.integer :show_id
+      c.integer :actor_id
     end
   end
 end
