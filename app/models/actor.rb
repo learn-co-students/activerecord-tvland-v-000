661204401ee5686691ea60f684e018bcc9ea4767
Rebecca Tong <rtong@buffalo.edu>
2018-05-11 17:55:59 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    [self.first_name, self.last_name].join(' ')
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+      [character.name, Show.find_by(id: character.show_id).name].join(' - ')
+    end
+  end
+
 end
