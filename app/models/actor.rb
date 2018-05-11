fa8e4973de481bb2584ec046d61f2ed0df01ded0
Ryan Manchester <ryanm17@gmail.com>
2018-05-11 18:04:58 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,12 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    puts "#{self.first_name} #{self.last_name}"
+   "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    binding.pry
+    char = self.characters.collect {|character| character.name}
+    show = self.shows.collect {|show| show.name}
   end
 end
