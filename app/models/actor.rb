a8d83fb4ba9bd1b51a9e3dd0b18d2580ae0f0529
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-11 19:58:07 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,10 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    
+  end
 end
