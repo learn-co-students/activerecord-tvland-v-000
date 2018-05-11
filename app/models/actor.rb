01cccd7262f4c119dca834b66c4b53516a5154bf
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-11 20:44:04 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,8 +3,11 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    first = self.first_name
-    last = self.last_name
-    
+    "#{self.first_name} #{self.last_name}"
   end
+
+  def list_roles
+    "#{self.characters.select(:name)} - #{self.shows}"
+  end
+
 end
