449aaba18e8b82fe47c4f6133c29adb803afc975
Victoria Meng <nights1ststar@protonmail.com>
2018-05-14 01:42:38 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+  end
+
+  def build_show(show)
+    show = Show.create(show)
+    self.show = show
+  end
 end
