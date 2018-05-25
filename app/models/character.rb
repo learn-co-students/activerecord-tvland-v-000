701983c228cbfa7f9167755f8b9d093095130610
Ally Netzel <netzelally@gmail.com>
2018-05-25 17:22:38 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
-
-  def catchphrase
-    "#{self.name}- #{self.actor}"
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
   end
 end
