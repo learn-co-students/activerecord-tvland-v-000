761fa33f5747f715b2303a6b8e55926b7f897cc1
Suchitra Naik <susanaik@gmail.com>
2018-06-04 21:31:45 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,28 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def character_name
+    ch_name = []
+    ch_name = characters.map do |c|
+      c.name
+    end
+    ch_name.join
+  end
+
+  def show_name
+    sh_name = []
+    sh_name = shows.map do |s|
+      s.name
+    end
+    sh_name.join
+  end
+
+  def list_roles
+    "#{character_name} - #{show_name}"
+  end
 end
