b7dc2393abf5911aec59c933e8c915a11d8c9be0
Adam Kalt <adam.kalt.1@gmail.com>
2018-06-07 18:46:58 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 5) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
     t.string   "value"
@@ -20,6 +25,11 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   add_index "ar_internal_metadata", ["key"], name: "sqlite_autoindex_ar_internal_metadata_1", unique: true
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "show_id"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string  "call_letters"
