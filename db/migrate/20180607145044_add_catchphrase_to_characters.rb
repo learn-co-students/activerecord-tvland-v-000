f442309bf1ffc341b8cd300e8e8f267d26b05344
Alex Wilson <fonzmp@gmail.com>
2018-06-07 15:46:52 +0000

    Done.

--- db/migrate/20180607145044_add_catchphrase_to_characters.rb
+++ db/migrate/20180607145044_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
-     class AddCatchphraseToCharacters
-< ActiveRecord::Migration[5.1]
-       def change
-       end
-     end
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
+   def change
+     add_column :characters, :catchphrase, :string
+   end
+end
