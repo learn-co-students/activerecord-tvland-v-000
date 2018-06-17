42fe72548b48de9aacc17b0fe898ff7d52650243
Ron Sala <ron.sala@gmail.com>
2018-06-17 06:21:38 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,10 +2,16 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  # attr_accessor: first_name, last_name
-
   def full_name
-    # actor = self.find_by first_name:
     self.first_name + ' ' + self.last_name
+  end
+
+  def list_roles
+    characters = self.characters
+    characters.each do |c|
+      c.shows
+    end
+    # self.shows
+    # binding.pry
   end
 end
--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -1,4 +1,4 @@
-class AddShowsToNetwork < ActiveRecord::Migration [4.2]
+class AddShowsToNetwork < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :network_id, :integer
   end
