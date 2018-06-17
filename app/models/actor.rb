02d787188eb4cfa2689bb0bf405ead0ce18e27fa
Ron Sala <ron.sala@gmail.com>
2018-06-17 18:58:20 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,11 +7,17 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
+    # TODO: build a method on actor that will return an array of
+    # strings in the form "#{character_name} - #{show_name}"
+    c_s = []
     characters = self.characters
     characters.each do |c|
-      c.shows
+      character_name = c.name
+      s_id = c.show_id
+      show_name = Show.find(s_id).name
+      c_s << "#{character_name} - #{show_name}"
+      # binding.pry
     end
-    # self.shows
-    # binding.pry
+    c_s
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
   has_many :characters
-  has_many :shows, through: :characters
+  has_many :shows, {:through=>:characters, :source=>"show"}
   belongs_to :actor
   belongs_to :show
 end
