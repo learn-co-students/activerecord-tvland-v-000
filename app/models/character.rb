015b8fd73bb92b0a136e981dd2ea24c52d51e75d
Christopher P. Dundon <chris.dundon@gmail.com>
2018-06-16 22:40:10 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,15 @@
 class Character < ActiveRecord::Base
 	belongs_to :actor
-  belongs_to :show 
+  belongs_to :show
+
+	def say_that_thing_you_say
+		"#{self.name} always says: #{self.catchphrase}"
+	end 
+
+	def build_show(instance_data)
+		show = Show.new(instance_data)
+		show.characters << self
+		show.save
+		show
+	end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,12 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+	belongs_to :network
+
+	 def build_network(instance_data)
+    net = Network.new(instance_data)
+    net.shows << self
+    net.save
+		net
+  end
 end
