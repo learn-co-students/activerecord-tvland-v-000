3b60fb594655d1925c269d053df09aa29ca013dc
Ron Sala <ron.sala@gmail.com>
2018-06-17 02:19:54 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,11 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+
+  # attr_accessor: first_name, last_name
+
+  def full_name
+    # actor = self.find_by first_name:
+    self.first_name + ' ' + self.last_name
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,6 @@
 class Character < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  belongs_to :actor
+  belongs_to :show
 end
