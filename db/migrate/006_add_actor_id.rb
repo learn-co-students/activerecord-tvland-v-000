23cafd40faa562aa4cde4ae49b67c9670664027e
Matt Farmer <mrfarmer777@gmail.com>
2018-06-18 03:19:41 +0000

    Done.

---
+++ db/migrate/006_add_actor_id.rb
@@ -0,0 +1,5 @@
+class AddActorId<ActiveRecord::Migration[4.2]
+  def change
+    add_column :characters, :actor_id, :integer
+  end
+end 
---
+++ db/migrate/009_add_day_and_season.rb
@@ -0,0 +1,7 @@
+class AddDayAndSeason<ActiveRecord::Migration[4.2]
+  def change
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+
+  end
+end
