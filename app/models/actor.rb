a19fc51b9e93daf4f02ef20658684e02d1613624
Christiana Vandermale <christianavdm@gmail.com>
2018-06-19 23:00:19 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,8 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :shows
+  has_many :characters, through: :shows
+
+  def full_name
+    puts "#{self.first_name} #{self.last_name}"
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+
 end
