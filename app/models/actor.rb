ead5f0753716d6c3e8ffefde8554489056bcaac2
Shirley Collins <shirley@m104.us>
2018-06-20 11:10:10 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    name = Array.new
+    name << self.first_name
+    name << self.last_name
+    name.join(" ")
+  end
+
+  def list_roles
+    roles = self.characters.map {|x| "#{x.name} - #{x.show.name}"}
+  end
 end
