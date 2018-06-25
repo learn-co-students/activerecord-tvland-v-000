7aeb5b0963e0c74a935dfe38a553deb56946d0a5
Anu Khambete <anukhambete@gmail.com>
2018-06-25 19:38:05 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+    #binding.pry
+  end
+
+  def list_roles
+    #binding.pry
+    self.characters
+  end
+
+
 end
