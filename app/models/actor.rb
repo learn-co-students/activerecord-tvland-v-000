e877b30fd5942338f899d26fc43f6723d5eee6aa
Emily S <emily.sugano@gmail.com>
2018-06-26 17:56:32 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,12 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    puts "#{self.first_name} #{self.last_name}"
+     "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end
 end
--- db/migrate/006_add_characters_to_actors.rb
+++
@@ -1,5 +0,0 @@
-class AddCharactersToActors < ActiveRecord::Migration[4.2]
-  def change
-    add_column :actors, :character_id, :integer
-  end
-end
