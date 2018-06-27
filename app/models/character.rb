80960c36fa6786f9f66228f7cdaefeaefbd02ae6
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-27 23:17:38 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
-  
+
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+
 end
