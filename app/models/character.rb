79e96dec0a955a9c3a64dbc1854c67db07b2d157
Gabriel Valle <gvalle91@gmail.com>
2018-06-30 22:02:12 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(hash)
+    #binding.pry
+    new_show = Show.create(name: hash[:name])
+    self.show_id = new_show.id
+    new_show
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(hash)
+    new_network = Network.create(call_letters: hash[:call_letters])
+    self.network_id = new_network.id
+    new_network
+  end
 end
