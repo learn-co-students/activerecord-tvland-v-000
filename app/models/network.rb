1727a6d3632f9b6555fae2b45475cb24d85e471b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-02 23:19:04 +0000

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,6 @@
 class Network < ActiveRecord::Base
   has_many :shows
-  has_many :characters, through: :shows
-  has_many :actors, through: :characters
+  
 
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,5 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
-  has_many :actors, through: :characters
-
-  def build_network(arg)
-
-  end
 
 end
