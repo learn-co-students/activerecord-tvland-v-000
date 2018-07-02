e67ce0b54dbf50cbccd73f170a754ca2f2c76c38
Veronica Bustabad <vbustabad@gmail.com>
2018-07-02 23:58:28 +0000

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,14 @@
 class Network < ActiveRecord::Base
+
   has_many :shows
+  has_many :characters, through: :shows
+
+  def say_that_thing_you_say
+    "#{character.name} always says: #{character.catchphrase}"
+  end 
 
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
   end
+
 end
