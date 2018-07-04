8cb3968d7700ed336c0cceced619eee9e9dced2c
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-04 22:11:18 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  # def network
+  #   Network.find(self.network_id)
+  # end
+  #
+  # def build_network(call_letters)
+  #   self.network = Network.new(call_letters)
+  # end
 end
