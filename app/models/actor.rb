24c5521e1289032cfdf1ab05eab88fee0538eec1
Elaine Anderson <eparie@bu.edu>
2018-07-05 22:05:03 +0000

    completed lab

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    "#{first} #{last}"
+  end
+
+  def list_roles
+    self.characters.collect do |n|
+    character = n.name
+    show = n.show.name
+    "#{character} - #{show}"
+      end
+    end
+
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def self.build_network(letters)
+    network.call_letters = letters
+  end
 end
