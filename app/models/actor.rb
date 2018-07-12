236a8f4e17afe6ecca4dd97a95803884e39d172c
Monica Seaman <monica.l.seaman@gmail.com>
2018-07-12 19:45:11 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    #self.characters.collect do |character|
+    #  "#{self.characters.name} - #{self.characters.shows.name}"
+    #end
+    "#{self.characters.first.name} - #{self.shows.first.name}"
+  end
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
