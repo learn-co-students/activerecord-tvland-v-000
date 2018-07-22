9e0f506c1d26e14198fab93266430184efe5c2cb
Kevin Chi <kevinchi118@gmail.com>
2018-07-22 01:15:52 +0000

    1

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network 
+
+    def build_network(hash)
+        Network.create(hash)
+    end
 end
