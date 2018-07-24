1d9bf5fc73419ec1cb706d0704288e1a9734ad0f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-24 13:43:30 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+  def full_name
+    puts "#{self.first_name} #{self.last_name}"
+
+  end
+
+
+
 end
