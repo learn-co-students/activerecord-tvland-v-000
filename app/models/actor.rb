6ddb7932e847c999b7e0d4457bf0723ee318691b
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-02 03:03:07 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        "Khaleesi - Game of Thrones"
+        "#{self.characters} - #{self.shows}"
+    end
 end
