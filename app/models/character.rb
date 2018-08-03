278b6a3e137c7ce84a1d946672dde768a5f19d03
ValerieHMoore <periwinkle17@outlook.com>
2018-08-03 11:38:53 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_network(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
 end
