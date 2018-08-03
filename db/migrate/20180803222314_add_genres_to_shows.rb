0571c81016d0c302b4147a23b9354ace44ce6025
Nate Miller <natekmiller96@gmail.com>
2018-08-03 22:51:21 +0000

    Done.

---
+++ db/migrate/20180803222314_add_genres_to_shows.rb
@@ -0,0 +1,7 @@
+class AddGenresToShows < ActiveRecord::Migration[5.1]
+  def change
+    add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+  end
+end
