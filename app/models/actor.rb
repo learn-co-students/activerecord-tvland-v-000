967b3cd11fd200fcd5e6a09af50cc58808c98a8a
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-07 10:04:02 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = self.first_name + " " + self.last_name
+    full_name
+  end
+
+  def list_roles
+    roles = []
+    self.characters.each_with_index do |character, index|
+      roles << "#{character.name} - #{self.shows[index].name}"
+    end
+    roles
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def build_network(call_letters:)
+    network = Network.new(call_letters: call_letters)
+    self.network_id = network.id
+  end
+
 end
