102f38962889a95319500667993e943071d1feda
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-21 21:43:27 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,9 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
   #A character belongs to an actor and belongs to a show.
+
+  def say_that_thing_you_say
+    "As #{self.name} always says: #{self.catchphrase}"
+  end
+  #Define a method in the Character class, #say_that_thing_you_say, using a given character's catchphrase.
 end
