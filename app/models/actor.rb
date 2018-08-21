2659a3d25c9635c9fce2259b736522a698c9fb9d
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-21 21:30:15 +0000

    first commit

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+  #An actor has many characters and has many shows through characters.
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+    #...that returns the first and last name of an actor.
+  end
+
+  def list_roles
+    self.characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+      end
+    #...that lists all of the characters that actor has.
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  #A character belongs to an actor and belongs to a show.
 end
