96ec3ce68a32e6684d7ef990ea973d7e3d5ff92b
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-21 22:36:24 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  # Associate the Actor model with the Character and Show model
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end 
+  
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end 
+  end 
   
 end
