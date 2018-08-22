49ba680ba3d8b6cab023430315e0a886a80c4666
Emily J Cain <emilycain@ymail.com>
2018-08-22 17:45:59 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,8 @@ class Actor < ActiveRecord::Base
   def full_name
     "#{self.first_name} #{self.last_name}"
   end 
+  
+  def list_roles 
+    self.characters.map {|c| "#{c.name} - #{c.show.name}"}
+  end 
 end
