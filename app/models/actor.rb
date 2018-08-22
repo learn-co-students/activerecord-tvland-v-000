430a0127f961c786c79440139850d1876180c00f
Madeline Stark <madeline.stark1@gmail.com>
2018-08-22 15:46:08 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+    #instance method b/c just working with one instance
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+
 end
