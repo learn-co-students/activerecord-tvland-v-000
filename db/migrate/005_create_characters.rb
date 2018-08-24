7541ab381ed8143a6f9d48fbc88d3e6efc16e456
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 00:18:31 +0000

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,8 +3,6 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
     create_table :characters do |t|
       t.string :name
       t.integer :actor_id
-      t.integer :show_id
-      t.integer :catchphrase
     end
   end
 end
