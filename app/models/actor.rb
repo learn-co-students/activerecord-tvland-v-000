7959eb73b02ce3a2fce2220cd78787e579d11e91
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-28 02:10:12 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,5 +9,6 @@ class Actor < ActiveRecord::Base
   def list_roles
     characters.collect do |character|
       "#{character.name} - #{character.show.name}"
+    end
   end
 end
--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -4,4 +4,5 @@ class CreateActors < ActiveRecord::Migration[4.2]
       t.string :first_name
       t.string :last_name
     end
+  end
 end
