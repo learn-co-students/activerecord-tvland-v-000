38681a0db31d865e0d09292b0b82578ea0221d81
Brandon Green <brandon@brandonleeproductions.com>
2018-08-28 01:54:43 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network
+    #binding.pry
+    self.network
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 5) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
+    t.integer "actor_id"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
@@ -30,6 +32,9 @@ ActiveRecord::Schema.define(version: 5) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
