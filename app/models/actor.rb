ee5da6a6690f52d04cd198e6111cdd139d2cd79e
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-01 22:34:10 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,19 @@
-class Actor < ActiveRecord::Base
+require 'pry'
 
-  
+class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.actors.first_name
+    self.actors.last_name
+  end
+
+  def list_roles
+    #binding.pry
+    self.actors.characters
+    
+  end
+
 
 end
