5c5cef7f20cdd5de3408cc9d40d8d07567baa5b0
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-09-01 21:24:21 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,6 @@
 class Character < ActiveRecord::Base
-  
+
+  belongs_to :shows
+  belongs_to :actors
+
 end
---
+++ db/migrate/006_add_characters_to_actors.rb
@@ -0,0 +1,6 @@
+class AddCharactersToActors < ActiveRecord::Migration[4.2]
+
+  def change
+    add_column :actors, :character_id, :integer
+  end
+end
