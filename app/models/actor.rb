7e73a1edc03dd2403d4020a6f39ce1c684fbad4d
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-02 16:35:11 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,14 +5,14 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.actors.first_name
-    self.actors.last_name
+    "#{self.actors.first_name } #{self.actors.last_name}"
+
   end
 
   def list_roles
     #binding.pry
     self.actors.characters
-    
+
   end
 
 
