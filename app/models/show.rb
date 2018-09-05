e51a28b7de731e652c140d3c88472489924aa519
Katrina Pritchard <pritckat@gmail.com>
2018-09-05 19:02:03 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,6 @@
 class Show < ActiveRecord::Base
   has_many :characters
+  has_many :actors, through: :characters
   belongs_to :network
-
-  def build_network(arg)
-  end
-
-  def call_letters
-  end
 
 end
