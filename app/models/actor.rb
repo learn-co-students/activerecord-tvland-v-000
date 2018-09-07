87db9f40146c294dae25f52ae733eba025f96827
Ethel C <cofresi.ethel@gmail.com>
2018-09-07 13:15:33 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
 class Actor < ActiveRecord::Base
   
-end
+  has_many :characters
+  has_many :shows, through: :characters
+  
+# TODO create an instance method on actor called full_name to return first and
+#last name together
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+# TODO create a list_roles method
+# TODO: build a method on actor that will return an array of
+# strings in the form "#{character_name} - #{show_name}"
+
+  def list_roles
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+end #end class
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,7 @@
 class Network < ActiveRecord::Base
   has_many :shows
-
+  
+#  it "should have picked up John Mulaney's Pilot" do
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
   end
