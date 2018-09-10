4136a96117b09ba582dc9a5d6e2a666e39b751cd
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-10 02:55:53 +0000

    Done.

---
+++ db/migrate/008_add_columns_to_characters.rb
@@ -0,0 +1,9 @@
+class AddColumnsToCharacters < ActiveRecord::Migration[4.2]
+  #to make an additive change to a schema we create
+  #a new migration, and then in the change method,
+  #we make the change.
+  def change
+    add_column :characters, :actor_id, :integer
+    add_column :characters, :catchphrase, :string
+  end
+end
