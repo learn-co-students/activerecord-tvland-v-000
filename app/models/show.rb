10170e6665c04b22feaf41b6a8f8d52283577b45
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-10-02 12:36:00 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,9 @@
 class Show < ActiveRecord::Base
+  belongs_to :network
   has_many :characters
   has_many :actors, through: :characters
+
+  def build_network(call_letters)
+    Network.create(call_letters: nil)
+  end
 end
