8c1848c462d87affceef903830c7ce4f1ef3df22
Adam Barton <adam.barton333@gmail.com>
2018-10-02 23:33:46 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |c|
+      "#{c.name} - #{c.show.name}"
+      # c.show.collect do |s|
+      #   "c.name - s.name"
+      # end
+    end
+  end
+
 end
---
+++ db/migrate/007_update_shows.rb
@@ -0,0 +1,5 @@
+class UpdateShows < ActiveRecord::Migration[4.2]
+  def change
+    add_column :shows, :genre, :string
+  end
+end
