faba985230e5b0254d35bcb6bb6ed8772f6326e1
Barbara Choy <barbchoy@gmail.com>
2018-10-17 02:39:42 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,10 @@
 class Actor < ActiveRecord::Base
-  
+
+  has_many :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+
 end
---
+++ db/migrate/004_create_actors.rb
@@ -0,0 +1,13 @@
+class CreateActors < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :actor_id
+    end
+  end
+
+
+
+end
