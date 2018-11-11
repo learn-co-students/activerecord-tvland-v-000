4236c49d55c739faad8257a89d0420db2be8cf05
Mohammed Noori <kmanoorib@gmail.com>
2018-11-11 19:22:56 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name # NOT a Class Method, don't use .self for def
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    # self.characters.all
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
   
 end
