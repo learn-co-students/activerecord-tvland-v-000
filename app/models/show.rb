b0e24460376a883a57fd4cf630f7986e90bc1614
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-11-14 21:04:47 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
+  belongs_to :genre
 
   def build_network(name)
-    binding.pry
-
+    self.network  = Network.create(name)
   end
+
 end
