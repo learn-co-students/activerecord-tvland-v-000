89f2d5d43c2412eaa133eab0c539db6e5dad0fe0
John Werenski <jtwerenski@gmail.com>
2018-11-19 18:04:31 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name 
+    "#{self.first_name} #{self.last_name}"
+  end 
+  
+  def list_roles 
+    #binding.pry
+    self.characters.collect {|c| "#{c.name} - #{c.show.name}" }
+  end 
 end
