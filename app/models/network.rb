34b620039ea794a00918be555af3c94930d5f121
Meg Gutshall <meghan.gutshall@gmail.com>
2018-11-25 15:40:28 -0500

    Done.

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,9 @@
 class Network < ActiveRecord::Base
   has_many :shows
+  has_many :characters, through: :shows
 
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
   end
+  
 end
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :characters do |c|
+      c.string :name
+      c.integer :show_id
+    end
+  end
+
+end
