e1982a38037ac8e072a9f6797e56a71a6fac86d6
Greg Morgan <gregm9999@gmail.com>
2018-11-25 03:05:21 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,11 +1,9 @@
-require"pry"
 class Character < ActiveRecord::Base
 
   belongs_to :actor
   belongs_to :show
 
   def say_that_thing_you_say
-    # binding.pry
     "#{self.name} always says: #{self.catchphrase}"
   end
 
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,13 +1,8 @@
-require "pry"
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
-    binding.pry
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
-
-      # t.integer :actor_id
-      # t.text :catchphrase
     end
   end
 
