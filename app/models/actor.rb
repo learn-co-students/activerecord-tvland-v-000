193db0b7d993b84c6b4763928e2931b862f3b0f2
Heather Miller <heather.miller114@gmail.com>
2018-11-27 22:33:04 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+  
+  def list_roles
+    self.characters = characters
+    self.shows = shows
+    names = []
+    show_name = []
+    characters.collect do |character|
+      names << character.name
+    end
+    shows.collect do |show|
+      show_name << show.name
+    end
+    names[0] + " - " + show_name[0]
+  end
 end
