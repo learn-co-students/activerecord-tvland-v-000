4f56dc2ecb9daeb123f813a7262f48d7dafdf534
Jason Mei <jason.mei403@gmail.com>
2018-11-28 17:39:30 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 8) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 8) do
     t.string "name"
     t.integer "network_id"
     t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
