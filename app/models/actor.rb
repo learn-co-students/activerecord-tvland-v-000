fcc065d07d1d0e88138f7cb22855df64af21b589
Erin Green <erin.y.green@gmail.com>
2018-12-01 22:50:03 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,11 @@
 class Actor < ActiveRecord::Base
+  has_many :characters, through: :shows
   
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    self.characters
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
+  belongs_to :actors 
+  belongs_to :shows
   
+  def say_that_thing_you_say
+    self.catchphrase 
+  end
 end
