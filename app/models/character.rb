1ea943d7d07a8b397cbabdf460cd508fd2483fdd
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-12-02 06:32:35 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
   
+  def say_that_thing_you_say 
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+  
+  def build_show(options = {})
+    new_show = Show.create(name: options[name])
+    self.show = new_show
+  end
+  
+  def build_network(options = {})
+    self.network = Network.create(call_letters: options[call_letters])
+  end
 end
