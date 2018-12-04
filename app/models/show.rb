26a683feb7e9094632a2b479f9720da36dac5775
Mike Dilley <mike.dilley@gmail.com>
2018-12-04 23:26:33 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(args)
+    self.network = Network.find_or_create_by(args)
+  end
+
 end
