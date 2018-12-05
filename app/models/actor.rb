7364d5e42547de6b41989a4e815dd3d6db2ab699
Rafael Lopez <lpz.raf@gmail.com>
2018-12-05 02:07:03 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,12 +8,7 @@ class Actor < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
   
-   #build a method on actor that will return an array of
-    # strings in the form "#{character_name} - #{show_name}"
-    # "characters.name - show.name"
-  
   def list_roles
-    #binding.pry
      self.characters.collect do |charac|
         "#{charac.name} - #{charac.show.name}"
       end
