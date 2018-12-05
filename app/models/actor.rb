b488d0a4e21e33ac9f3ef92974827fb303dcbbd2
Rafael Lopez <lpz.raf@gmail.com>
2018-12-05 02:06:34 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
@@ -6,10 +8,15 @@ class Actor < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
   
+   #build a method on actor that will return an array of
+    # strings in the form "#{character_name} - #{show_name}"
+    # "characters.name - show.name"
+  
   def list_roles
-    #i have it wrong
-    self.shows 
-    self.characters
+    #binding.pry
+     self.characters.collect do |charac|
+        "#{charac.name} - #{charac.show.name}"
+      end
   end
   
 end
