29ff4948ae20d15a4deb276b156f04dcbb40a44f
Tom  Cho <tc.thomascho@gmail.com>
2018-12-13 23:37:02 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters #a character belongs_to an actor
+  has_many :shows, through: :characters #a character belongs_to a show, or, each character that belong to an actor also has a show
+
+  def full_name
+    "#{self.first_name}" + " #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect {|character|
+      "#{character.name} - #{character.show.name}"
+    }
+  end
 end
