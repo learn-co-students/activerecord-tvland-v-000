efb63b6beea7ec922ff27d9553b7496a8e75ad4e
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-28 20:54:38 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  has_many :characters 
+  has_many :shows, through: :characters 
   
+  def full_name 
+    return "#{first_name} #{last_name}"
+  end 
+  
+  def list_roles 
+    array = []
+    self.characters.each do |role|
+      array << "#{role.name} - #{role.show.name}"
+    end 
+    array 
+  end 
 end
