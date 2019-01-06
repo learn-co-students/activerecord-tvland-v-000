934eab0c467f693fb4dcc597f4e2def24a6c030e
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-01-06 22:03:22 +0000

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :characters do |t|
+            t.string :name
+            t.integer :show_id
+        end
+    end
+
+end
