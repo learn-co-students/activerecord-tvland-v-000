65af834811e4e7665d48bf821f04743429ce7577
Stef Claus <stef.claus@gmail.com>
2019-01-26 19:13:38 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
 
   def list_roles
-    self.characters.map{|character| character.name}
+    self.characters.map{|character| "#{character.name} - #{character.show.name}"}
   end
 
 
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,5 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  belongs_to :genre
+  has_many :actors, through: :characters
+  belongs_to :network
 end
