0086b2ea957021c602845c102fddb5ac016f4ee1
Stef Claus <stef.claus@gmail.com>
2019-01-26 18:32:21 +0000

    Done.

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,4 +1,4 @@
-class CreateActors < ActiveRecord::Migration
+class CreateActors < ActiveRecord::Migration[4.2]
   def change
     create_table :actors do |t|
       #primary key of :id is created for us!
