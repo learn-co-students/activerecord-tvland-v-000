510de599a668e82383f2b4006973d286e6cbe307
Stef Claus <stef.claus@gmail.com>
2019-01-26 18:31:00 +0000

    Done.

--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -1,4 +1,4 @@
-class AddShowsToNetwork < ActiveRecord::Migration
+class CreateActors < ActiveRecord::Migration
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
