678d80e7d6db14791a2ee42edb28fe7caf0dff19
Cheryl K <cherylkelson@icloud.com>
2019-01-29 20:24:18 +0000

    Done.

--- db/migrate/005_create_actors.rb
+++ db/migrate/005_create_actors.rb
@@ -4,7 +4,10 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |t|
       t.string  :first_name
       t.string  :last_name
-    
+
       end
   end
-end
+
+  def full_name
+    '#{first_name}, #{last_name}
+  end
