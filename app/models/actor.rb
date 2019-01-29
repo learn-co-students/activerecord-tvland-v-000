76e118a0522f02cef9bfe2f782cb39f84a67dff3
Cheryl K <cherylkelson@icloud.com>
2019-01-29 01:48:42 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has-many :shows, through :: characters
+
+  # def actors
+  # change-table actors
+  # t: string, first_name
+  # t: string, last_name
+  # end
+
+
+
+
+
 end
