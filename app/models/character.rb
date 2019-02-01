6aa42a6321a02aa3cf7e2739642ecc9417c5c572
Ashley Wong <ashwong04@gmail.com>
2019-02-01 00:24:51 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
   belongs_to :actor
-  belongs_to :character
+  belongs_to :show
   
   def say_that_thing_you_say
   end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
+  has_many :character
+  has_many :actor, through: :character
+  
+  
   
 end
