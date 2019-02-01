f69a6b2a3e86e137eafcee29e3ce04c1124b5315
Ashley Wong <ashwong04@gmail.com>
2019-02-01 00:46:27 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ActiveRecord::Base
-  belongs_to :actors
-  belongs_to :shows
+  belongs_to :actor
+  belongs_to :show
   
   def say_that_thing_you_say
     self.actor.catchphrase
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,6 +4,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
+      t.string :catchphrase
     end
   end
   
