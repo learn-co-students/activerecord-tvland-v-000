ab1d38428d60ed33aaa891f17b6bdaadfcdade3b
Lois Lee <lois.lee@me.com>
2019-02-11 21:20:05 +0000

    Done.

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,11 @@
-class AddShowsToNetwork < ActiveRecord::Migration[5.1]
+class CreateActors < ActiveRecord::Migration[5.1]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :shows, :network_id, :integer
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
---
+++ db/migrate/009_add_season_to_shows.rb
@@ -0,0 +1,7 @@
+class AddSeasonToShows < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :shows, :season, :string
+  end
+  
+end
