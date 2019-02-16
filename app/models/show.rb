47b1372ebe859c976ba72c789febfe60c0b50b07
Marie Burns <mcburns2222@gmail.com>
2019-02-16 21:48:52 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Show < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def build_network(arg = {})
+    network = Network.new(arg)
+    network.shows << self 
+    network.shows
+  end
 end
