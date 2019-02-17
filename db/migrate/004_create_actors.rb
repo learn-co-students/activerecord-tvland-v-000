1854e4a2508708286a9053805232b3e73844e1bd
Lauren Pardee <lap5151@gmail.com>
2019-02-17 17:30:57 +0000

    Done.

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,4 +1,4 @@
-class CreateActors < ActiveRecord::Migration[4.2]
+class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t|
       t.string :first_name
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,4 +1,4 @@
-class CreateCharacters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string  :name
