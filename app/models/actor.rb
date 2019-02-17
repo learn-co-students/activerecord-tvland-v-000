6c112e85d5f4b441624fdd347bd45a87dead0df9
Lauren Pardee <lap5151@gmail.com>
2019-02-17 17:17:19 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = self.first_name + " " + self.last_name
+    full_name
+  end
+
+
 end
