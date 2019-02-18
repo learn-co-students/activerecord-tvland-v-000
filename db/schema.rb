d2f1bcb00a497b7744396d02d6776337061ac099
tgray017 <tgray017@gmail.com>
2019-02-18 15:08:42 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 6) do
     t.string "name"
     t.integer "show_id"
     t.integer "actor_id"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
