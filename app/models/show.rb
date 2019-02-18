8e30b34e4b44f7376c3f2a3e869212d501965a04
tgray017 <tgray017@gmail.com>
2019-02-18 15:26:07 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
   
   def build_network(attributes)
     network = Network.new(attributes)
