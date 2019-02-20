a40909417a44deb1093024ecb505838239f8ac11
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-20 20:47:11 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
   
   def say_that_thing_you_say
-    binding.pry
-    puts "#{self.name} always says: #{self.catchphrase}"
+    "#{self.name} always says: #{self.catchphrase}"
   end
 end
