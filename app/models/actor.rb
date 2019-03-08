ac76d15dac6dcd92a4b4d38545231c1eae62acd5
Monica Marziano <mmarziano1@gmail.com>
2019-03-08 00:34:34 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name 
+    return "#{self.first_name}" + " #{self.last_name}"
+  end 
+
+  def list_roles 
+    return "#{self.characters.name} - #{self.shows.name}"
+  end 
+
+
 end
+
+
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def build_network 
+    
+  end 
+
 end
