a56546e0d7ed0c726b03f122ab9a1558e49943c1
taisaoko <ttd3calvin@gmail.com>
2019-03-10 20:56:40 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,15 +6,18 @@ class Actor < ActiveRecord::Base
     name = "#{self.first_name} #{self.last_name}"
   end
 
-  # def list_roles
+  def list_roles
+    self.characters.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    end
+  end
+end
+
+
+
+
+ # def list_roles
   #   roles = 
   #   array_of_characters = self.characters.map {|character| character.name}
   #   array_of_shows = self.shows.map {|shows| shows.name}
   # end
-  
-  def list_roles
-    self.characters.collect do |character|
-     "#{character.name} - #{character.show.name}"
-    end.join
-  end
-end
