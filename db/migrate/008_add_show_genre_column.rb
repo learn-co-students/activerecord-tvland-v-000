1b211ed6be370dc7643e063bdd31f4fbad11bdce
ktochow1 <ktochow1@binghamton.edu>
2019-03-21 00:07:35 +0000

    Done.

---
+++ db/migrate/008_add_show_genre_column.rb
@@ -0,0 +1,12 @@
+class AddShowGenreColumn < ActiveRecord::Migration[4.2]
+
+  
+  def change 
+    add_column :shows, :day, :string 
+    add_column :shows, :season, :string 
+    add_column :shows, :genre, :string
+  end
+
+  
+  
+end
