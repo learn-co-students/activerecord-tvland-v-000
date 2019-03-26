96608ce193c20f2604b97425966a02bbdf90933b
Grace Choi <oiograceoio@gmail.com>
2019-03-26 13:47:53 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_network
+
+  end
 end
