b74a34e43d54b05e7959e49feb9aab1285eb8e5f
Amil Masic <amil.masic@gmail.com>
2019-04-16 11:40:07 +0000

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,6 +4,7 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
+      t.integer :actor_id
     end
   end
 
