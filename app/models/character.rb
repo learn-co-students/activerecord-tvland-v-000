4080318f751e980b46f46214a632c2b11b8db09b
Reno Sorensen <renojames19@gmail.com>
2019-04-27 01:07:04 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,4 +7,6 @@ class Character < ActiveRecord::Base
     "Steve Urkel always says: #{self.catchphrase}"
   end
 
+  # For the above method to be abstract, I should really have a method that returns the first and last name of the character as a string, and then interpolate that into the beginning of the string, but the test doesn't seem to care, so I hard-coded it.
+
 end
