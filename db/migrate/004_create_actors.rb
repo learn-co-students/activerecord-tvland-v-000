0ed6590d7633bfdeb1591b68b953244295f93ac4
Scott Silverman <scottms724@gmail.com>
2019-04-28 19:45:58 +0000

    Done.

---
+++ db/migrate/004_create_actors.rb
@@ -0,0 +1,12 @@
+class CreateActors < ActiveRecord::Migration[4.2]
+  
+  def change 
+    create_table :actors do |t|
+      t.string :first_name 
+      t.string :last_name 
+      t.integer :show_id 
+      t.integer :character_id 
+    end 
+  end 
+  
+end 
