9658a14a398c82003d41d7884eb6a9ab4bb78cc6
Karly Mareka <karly.mareka@gmail.com>
2019-04-29 01:35:43 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name
+    full_name = self.first_name + " " + self.last_name 
+  end
+  
+  def list_roles
+    self.characters.each do |character|
+      Show.all.each do |show|
+        show_name = nil
+        @character_role = nil 
+        if show.id == character.show_id
+          show_name = show.name
+          @character_role = "#{character.name} - #{show_name}"
+        end 
+      end 
+    end
+    @character_role 
+  end 
   
 end
