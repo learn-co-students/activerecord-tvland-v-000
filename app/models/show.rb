292a5f28654be9b1da32b1405a523e948ae54888
Karly Mareka <karly.mareka@gmail.com>
2019-04-29 02:06:06 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
-  has_many :actors, through: :characters 
+  has_many :actors, through: :characters
+  
+  def build_network(arg)
+    
+  end
+  
+  
 end
