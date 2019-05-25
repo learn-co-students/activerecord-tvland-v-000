65131ed8dacd8b24989feee974592f8e6bd8096f
matthew.serre <matthew.serre@gmail.com>
2019-05-25 20:25:25 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,11 +3,12 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
   
   def full_name
-    self.first_name
-    self.last_name
+    self.first_name + " " + self.last_name
   end
   
   def list_roles
-
+    self.characters.first.name + " - " + self.shows.first.name
   end
+  
+  
 end
--- db/migrate/006_add_actors_to_characters.rb
+++ db/migrate/006_add_actors_to_characters.rb
@@ -1,5 +1,5 @@
 class AddActorsToCharacters < ActiveRecord::Migration[4.2]
   def change
-    add_column(:characters, :actor_id, :integer)
+    add_column :characters, :actor_id, :integer
   end
 end
