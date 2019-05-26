2325400f54f465f3af638028c3bd258c5a29d185
Paul Fell <swellpf@gmail.com>
2019-05-26 17:56:32 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,8 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     characters.collect do |character|
-      "#{character.name}"
+      "#{character.name} - #{character.show.name}"
     end
   end
-
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,7 +3,6 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
-      t.integer :actor_id
     end
   end
 end
