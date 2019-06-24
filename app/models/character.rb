b969d3602f4f64f6f97b42258bb69c8ab4f33693
David Charles <ratlover03@gmail.com>
2019-06-24 22:55:02 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  belongs_to :network
+
+   def say_that_thing_you_say
+    return "#{self.name} always says: #{self.catchphrase}"
+    # binding.pry
+   end
+
+
 end
