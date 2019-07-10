66912515fa5fc8f1783e5925fbf1fb65c07fe5da
Karen Flores <karen.ag.flores@gmail.com>
2019-07-10 00:10:38 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,8 @@
 class Character < ActiveRecord::Base
-  belongs_to :shows
+  belongs_to :show
+  belongs_to :actor
 
   def say_that_thing_you_say
-    self.catchphrase
+    "#{self.name} always says: #{self.catchphrase}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 7) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 6) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "genre"
   end
 
 end
