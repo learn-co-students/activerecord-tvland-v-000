b6bbd7b7169ad4c8a2c22ae03100406f1773a483
Karen Flores <karen.ag.flores@gmail.com>
2019-07-10 23:41:04 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "character"
   end
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
     t.string "catchphrase"
+    t.integer "actor_id"
   end
 
   create_table "networks", force: :cascade do |t|
