fd33a5ba2fbc61e1d4b8ac1c608223c47e68eabd
Gigi M <mmgamao1@gmail.com>
2019-07-30 15:26:35 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
+
+# Write a migration for the characters table. A character should have a name, actor_id, and a show_id––a character will belong to a show (the show migration is already provided) and an actor, and we'll keep track of this relationship with these database table columns.Write a migration that adds the column catchphrase to your character model.
