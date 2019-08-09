08c95b1db94b03370f6658ee274ea603d2899e82
Adrienne Yao <adrienne.yao@icloud.com>
2019-08-09 03:18:00 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,10 @@ class Show < ActiveRecord::Base
   belongs_to :network
 
   def actors_list
-   self.actors.collect{|actor| actor.full_name}
+   self.actors.collect do |actor|
+    actor.full_name
+  end
  end
+
 
 end
