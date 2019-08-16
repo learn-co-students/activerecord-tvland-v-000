4741d47a90c63ee51db7e6c6bc7fd56a0fe22a85
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-16 05:18:43 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,5 @@
 class Actor < ActiveRecord::Base
+
   has_many :characters 
   has_many :shows, through: :characters
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 8) do
+
+  create_table "actors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "show_id"
+    t.string "catchphrase"
+    t.integer "actor_id"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
@@ -20,6 +32,9 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
