47e56492b73a6a625509b9089302635fa6df09bd
Alexis Kondilis <kondilisalexis@gmail.com>
2019-10-04 15:14:43 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    self.characters.collect do |character|
+      character.actor.full_name
+    end
+
+  end
 end
