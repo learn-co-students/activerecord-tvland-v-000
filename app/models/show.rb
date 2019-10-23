e88a5d80d1f0511c2648f89d8e88a038e80e3d61
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-23 02:48:58 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+  # def actors_list
+  #   actors.collect {|actor| "#{actor.name} - #{actor.show.name}"}
+  # end
 end
--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,10 @@
 class CreateCharacters  < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+      # t.string :last_name
     end
   end
 end
