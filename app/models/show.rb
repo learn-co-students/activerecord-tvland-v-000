a55adef2ef7963bb8d5ce4461b37fb547ed1dc2e
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 05:20:42 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    self.characters.map do |item|
+      item.name
+    end
+  end
+  
+  def 
 end
