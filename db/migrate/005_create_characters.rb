4bd35422b378a5a4f7b351b70aa3b9b2783555bc
Paris Dolfman <parisdolfman@gmail.com>
2020-10-16 14:24:19 +0000

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,8 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+  end
+end
