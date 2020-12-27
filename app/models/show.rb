10c88b6e78298f465cef59b0726d7400e4adeb94
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-27 21:19:17 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+
+  def actors_list
+    #that returns an Array of the full names of each actor
+    # associated with the a show.
+    # Remember, a show should have many actors through characters.
+  end
+
+
 end
---
+++ db/migrate/006_add_actors.rb
@@ -0,0 +1,12 @@
+
+class AddActors < ActiveRecord::Migration[5.1]
+  #define a change method in which to do the migration
+  def change
+    create_table :actors do |t|
+
+      t.string :first_name
+      t.string :last_name
+
+    end
+  end
+end
