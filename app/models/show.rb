f9cc8a170eff7517168f1b23b283948ccff98cf4
alejandra cantu <alejandra.cantu13@gmail.com>
2020-12-30 18:08:23 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,19 +1,19 @@
 class Show < ActiveRecord::Base
 
 belongs_to :network
-has_many :actors
 has_many :characters
+has_many :actors, through: :characters
 
   def actors_list
-    binding.pry
     #that returns an Array of the full names of each actor
     # associated with the a show.
     # Remember, a show should have many actors through characters.
-    self.actor_id.map do |n|
-       n.name
-binding.pry
 
+    self.characters.map do |c|
+      c.actor
 
+      list_actor_names = "#{c.actor.first_name} #{c.actor.last_name}"
+      list_actor_names
 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 11) do
+ActiveRecord::Schema.define(version: 10) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 11) do
   end
 
   create_table "characters", force: :cascade do |t|
+    t.string "name"
     t.integer "show_id"
     t.string "catchphrase"
-    t.string "name"
     t.integer "actor_id"
   end
 
@@ -29,7 +29,12 @@ ActiveRecord::Schema.define(version: 11) do
     t.integer "channel"
   end
 
-# Could not dump table "shows" because of following StandardError
-#   Unknown type 'strings' for column 'season'
+  create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.integer "network_id"
+    t.string "season"
+    t.string "day"
+    t.string "genre"
+  end
 
 end
