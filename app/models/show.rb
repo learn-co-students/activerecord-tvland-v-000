11527ddf084131d7058fe128d2dc36dabea80f10
Olivia Hanson <olivia.joy.hanson@gmail.com>
2021-03-02 14:01:38 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list 
+        self.actors.map{|a| "#{a.first_name} #{a.last_name}"}
+    end 
 end
