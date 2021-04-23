cc673f09dfe6450240ae6b0b89374ee5b4529f11
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-22 23:23:33 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+
+  def full_name
+    # self.first_name + last_name
+    p "#{first_name} #{last_name}"
+  end
 end
