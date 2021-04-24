a47855551b82e5c7225b334ef8b28894a8c71adf
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-24 00:11:13 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,4 @@ class Show < ActiveRecord::Base
   has_many :characters
 #   has_many :actors, through: :characters 
 
-  def 
-    self
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.string "name"
     t.integer "actor_id"
     t.integer "show_id"
+    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
