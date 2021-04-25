9e2f51599b4e80d613b9c599c4392a635e19da80
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-25 00:09:08 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,5 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters 
   belongs_to :network 
 
-
+  def actors_list
+    self.characters.map do |t|
+      t.actor.full_name
+    end
+  end
 end
